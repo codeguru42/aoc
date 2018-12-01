9836f4f94b8532395460a977b476949b5018df85
Code-Apprentice <codeguru42@gmail.com>
2018-11-30 22:14:30 -0700

    Solution for Day 1 Part 2

--- 2018/day1.py
+++ 2018/day1.py
@@ -3,8 +3,28 @@ def part1():
         return sum(int(line) for line in file)
 
 
+def partial_sums(l):
+    s = 0
+    while True:
+        for x in l:
+            yield s
+            s += x
+
+
+def part2():
+    frequencies = set()
+    with open('day1.txt') as file:
+        offsets = [int(x) for x in file]
+
+    for p in partial_sums(offsets):
+        if p in frequencies:
+            return p
+        frequencies.add(p)
+
+
 def main():
     print(part1())
+    print(part2())
 
 
 if __name__ == "__main__":
