fc4e7aeb0c01227ee0c93236c552fcbc8e51bea3
Code-Apprentice <codeguru42@gmail.com>
2018-12-02 02:12:18 -0700

    Day 2 Part 1

--- 2018/day2.py
+++ 2018/day2.py
@@ -1,5 +1,20 @@
+from collections import Counter
+
+
 def part1():
-    pass
+    with open('day2.txt') as file:
+        counts = [Counter(line) for line in file]
+
+        twos = 0
+        threes = 0
+
+        for c in counts:
+            if any(c[x] == 2 for x in c):
+                twos += 1
+            if any(c[x] == 3 for x in c):
+                threes += 1
+
+        return twos * threes
 
 
 def part2():
