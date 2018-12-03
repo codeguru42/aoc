52e6e766a103a7152890f9e5f99fc2a9a6a84ecf
Code-Apprentice <codeguru42@gmail.com>
2018-12-02 22:19:24 -0700

    Day 3 Part 1

--- 2018/day3.py
+++ 2018/day3.py
@@ -1,5 +1,30 @@
+def parse():
+    claims = []
+    with open('day3.txt') as file:
+        for line in file:
+            tokens = line.split()
+            claims.append({
+                'id': tokens[0][1:],
+                'loc': [int(x) for x in tokens[2][:-1].split(',')],
+                'size': [int(x) for x in tokens[3].split('x')],
+            })
+    return claims
+
+
 def part1():
-    pass
+    claims = parse()
+    overlap = set()
+    used = set()
+    for claim in claims:
+        left, top = claim['loc']
+        width, height = claim['size']
+        for x in range(left, left + width):
+            for y in range(top, top + height):
+                if (x, y) in used:
+                    overlap.add((x, y))
+                else:
+                    used.add((x, y))
+    return len(overlap)
 
 
 def part2():
