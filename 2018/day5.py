7884b3f87bd6cff880bbda5eb81e720500153e4a
Code-Apprentice <codeguru42@gmail.com>
2018-12-04 22:40:49 -0700

    Day 5 Part 1

--- 2018/day5.py
+++ 2018/day5.py
@@ -1,5 +1,28 @@
-def part1():
-    pass
+def read_input():
+    with open('day5.txt') as file:
+        return file.read()
+
+
+def part1(polymer):
+    start = ''
+    end = polymer
+
+    while end != '':
+        if start == '':
+            start = end[0]
+            end = end[1:]
+        else:
+            a = start[-1]
+            b = end[0]
+            if ((a.islower() and b.isupper())
+                or (a.isupper() and b.islower())) \
+                    and a.lower() == b.lower():
+                start = start[:-1]
+                end = end[1:]
+            else:
+                start = start + end[0]
+                end = end[1:]
+    return len(start)
 
 
 def part2():
@@ -7,7 +30,9 @@ def part2():
 
 
 def main():
-    print(part1())
+    assert part1('dabAcCaCBAcCcaDA') == 10
+    polymer = read_input().strip()
+    print(part1(polymer))
     print(part2())
 
 
