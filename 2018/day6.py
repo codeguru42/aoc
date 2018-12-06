414fa4a7c5267b00dd89aa38d223fbed336d3872
Code-Apprentice <codeguru42@gmail.com>
2018-12-05 22:06:24 -0700

    Day 6: Parse input file

--- 2018/day6.py
+++ 2018/day6.py
@@ -1,14 +1,20 @@
-def part1():
-    pass
+def parse():
+    with open('day6.txt') as file:
+        return [(int(x), int(y)) for x, y in [line.split(',') for line in file]]
 
 
-def part2():
-    pass
+def part1(points):
+    return points
+
+
+def part2(points):
+    return points
 
 
 def main():
-    print(part1())
-    print(part2())
+    points = parse()
+    print(part1(points))
+    print(part2(points))
 
 
 if __name__ == "__main__":
