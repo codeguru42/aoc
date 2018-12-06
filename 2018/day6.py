76c1dfad83382b58c4ddc4ce9f2f62d074b0ff8c
Code-Apprentice <codeguru42@gmail.com>
2018-12-06 12:45:23 -0700

    Day 6: Finish Part 1

--- 2018/day6.py
+++ 2018/day6.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from math import atan2
 
 
 def parse():
@@ -11,53 +10,48 @@ def ccw(p1, p2, p3):
     return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
 
 
-def angle_between(u, v):
-    x = (v[0] - u[0], v[1] - u[1])
-    return atan2(x[1], x[0])
-
-
-def convex_hull(points):
-    min_y = min(points, key=lambda p: p[1])
-    points.sort(key=lambda p: angle_between(min_y, p))
-    s = [points[0], points[1]]
-    for p in points[2:]:
-        while len(s) >= 2 and ccw(s[-2], s[-1], p) <= 0:
-            s.pop()
-        s.append(p)
-    return s
-
-
 def manhattan_distance(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
 
+def min_distance(p, points):
+    distances = [
+        {
+            'p': q,
+            'd': manhattan_distance(q, p),
+        }
+        for q in points
+    ]
+    min_dist = min(distances, key=lambda a: a['d'])
+    return [x for x in distances if x['d'] == min_dist['d']]
+
+
 def part1(points):
-    hull = convex_hull(points)
-    interior = set(points) - set(hull)
-    hull_coords = list(zip(*hull))
-    min_x = min(hull_coords[0])
-    max_x = max(hull_coords[0])
-    min_y = min(hull_coords[1])
-    max_y = max(hull_coords[1])
-    print(min_x, min_y, max_x, max_y)
+    coords = list(zip(*points))
+    min_x = min(coords[0])
+    max_x = max(coords[0])
+    min_y = min(coords[1])
+    max_y = max(coords[1])
+
+    infinite = set()
+    for x in range(min_x, max_x + 1):
+        for y in [min_y, max_y]:
+            min_dist_points = min_distance((x, y), points)
+            if len(min_dist_points) == 1:
+                infinite.add(min_dist_points[0]['p'])
+    for x in [min_x, max_x + 1]:
+        for y in range(min_y, max_y):
+            min_dist_points = min_distance((x, y), points)
+            if len(min_dist_points) == 1:
+                infinite.add(min_dist_points[0]['p'])
 
     areas = Counter()
-    for x in range(min_x, max_x + 1):
-        for y in range(min_y, max_y + 1):
-            distances = [
-                {
-                    'p': p,
-                    'd': manhattan_distance(p, (x, y)),
-                }
-                for p in points
-            ]
-            min_dist = min(distances, key=lambda a: a['d'])
-            points_at_d = [x['p'] for x in distances if x['d'] == min_dist['d']]
+    for x in range(min_x + 1, max_x):
+        for y in range(min_y + 1, max_y):
+            points_at_d = min_distance((x, y), points)
             if len(points_at_d) == 1:
-                areas[min_dist['p']] += 1
-    print('areas', areas)
-    interior_areas = [areas[p] for p in interior]
-    print('interior_areas', interior_areas)
+                areas[points_at_d[0]['p']] += 1
+    interior_areas = [areas[p] for p in points if p not in infinite]
     return max(interior_areas)
 
 
@@ -67,8 +61,7 @@ def part2(points):
 
 def main():
     example = [(1, 1), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9), ]
-    print(part1(example))
-    # assert part1(example) == 17
+    assert part1(example) == 17
     points = parse()
     print(part1(points))
     print(part2(points))
