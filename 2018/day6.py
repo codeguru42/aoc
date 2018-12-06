e4c55ca806584d64261de3b5af044f1850a4bfab
Code-Apprentice <codeguru42@gmail.com>
2018-12-05 23:22:03 -0700

    Day 6: Attempt at part 1

--- 2018/day6.py
+++ 2018/day6.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from math import atan2
 
 
@@ -26,9 +27,38 @@ def convex_hull(points):
     return s
 
 
+def manhattan_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
 def part1(points):
     hull = convex_hull(points)
-    return hull
+    interior = set(points) - set(hull)
+    hull_coords = list(zip(*hull))
+    min_x = min(hull_coords[0])
+    max_x = max(hull_coords[0])
+    min_y = min(hull_coords[1])
+    max_y = max(hull_coords[1])
+    print(min_x, min_y, max_x, max_y)
+
+    areas = Counter()
+    for x in range(min_x, max_x + 1):
+        for y in range(min_y, max_y + 1):
+            distances = [
+                {
+                    'p': p,
+                    'd': manhattan_distance(p, (x, y)),
+                }
+                for p in points
+            ]
+            min_dist = min(distances, key=lambda a: a['d'])
+            points_at_d = [x['p'] for x in distances if x['d'] == min_dist['d']]
+            if len(points_at_d) == 1:
+                areas[min_dist['p']] += 1
+    print('areas', areas)
+    interior_areas = [areas[p] for p in interior]
+    print('interior_areas', interior_areas)
+    return max(interior_areas)
 
 
 def part2(points):
@@ -36,6 +66,9 @@ def part2(points):
 
 
 def main():
+    example = [(1, 1), (1, 6), (8, 3), (3, 4), (5, 5), (8, 9), ]
+    print(part1(example))
+    # assert part1(example) == 17
     points = parse()
     print(part1(points))
     print(part2(points))
