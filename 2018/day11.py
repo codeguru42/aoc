ef8019ec6f688af560ee22890f118d0be45468cd
Code Apprentice <codeguru42@gmail.com>
2018-12-11 12:35:50 -0700

    Day 11: Implement calc_power()

--- 2018/day11.py
+++ 2018/day11.py
@@ -3,6 +3,12 @@ def parse():
         return file.readlines()
 
 
+def calc_power(x, y, grid_serial_number):
+    rack_id = x + 10
+    power = (rack_id * y + grid_serial_number) * rack_id
+    return (power // 100) % 10 - 5
+
+
 def part1(steps):
     return steps
 
@@ -12,6 +18,7 @@ def part2(steps):
 
 
 def main():
+    assert calc_power(3, 5, 8) == 4
     inp = parse()
     print(part1(inp))
     print(part2(inp))
