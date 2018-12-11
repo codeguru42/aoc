667d9475222e447a63a0e8381e51c7189d77f8b1
Code Apprentice <codeguru42@gmail.com>
2018-12-11 12:10:14 -0700

    Stub Day 11

---
+++ 2018/day11.py
@@ -0,0 +1,21 @@
+def parse():
+    with open('day8.txt') as file:
+        return file.readlines()
+
+
+def part1(steps):
+    return steps
+
+
+def part2(steps):
+    return steps
+
+
+def main():
+    inp = parse()
+    print(part1(inp))
+    print(part2(inp))
+
+
+if __name__ == "__main__":
+    main()
