f83ffa6aa0b616384702d3c981ca5f3b60a41543
Code Apprentice <codeguru42@gmail.com>
2018-12-11 13:35:54 -0700

    Day 11: Start to solve Part 1

--- 2018/day11.py
+++ 2018/day11.py
@@ -1,6 +1,6 @@
-def parse():
-    with open('day8.txt') as file:
-        return file.readlines()
+import itertools
+
+import numpy as np
 
 
 def calc_power(x, y, grid_serial_number):
@@ -9,19 +9,26 @@ def calc_power(x, y, grid_serial_number):
     return (power // 100) % 10 - 5
 
 
-def part1(steps):
-    return steps
+def part1(grid_serial_number):
+    coords = itertools.product(range(301), range(301))
+    power = [
+        calc_power(x, y, grid_serial_number) for x, y in coords
+    ]
+    power_2d = np.array(power)
+    power_2d.shape = (301, 301)
+    return power_2d
 
 
-def part2(steps):
-    return steps
+def part2():
+    return None
 
 
 def main():
     assert calc_power(3, 5, 8) == 4
-    inp = parse()
-    print(part1(inp))
-    print(part2(inp))
+
+    grid_serial_number = 1723
+    print(part1(grid_serial_number))
+    print(part2())
 
 
 if __name__ == "__main__":
