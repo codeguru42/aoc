7f9b94a67cd4f99f9269a8b2c4c5b674328187d7
Code-Apprentice <codeguru42@gmail.com>
2019-12-02 19:14:54 -0700

    AOC 2019, Day 1, Part 2

--- 2019/day01.py
+++ 2019/day01.py
@@ -2,13 +2,28 @@ def calc_fuel(mass):
     return mass // 3 - 2
 
 
+def calc_all_fuel(mass):
+    fuel = 0
+    mass = calc_fuel(mass)
+    while mass > 0:
+        fuel += mass
+        mass = calc_fuel(mass)
+    return fuel
+
+
 def part1(masses):
     return sum(calc_fuel(int(x)) for x in masses)
+
+
+def part2(masses):
+    return sum(calc_all_fuel(int(x)) for x in masses)
 
 
 def main():
     with open('day01.txt') as file:
         print(part1(file))
+    with open('day01.txt') as file:
+        print(part2(file))
 
 
 if __name__ == '__main__':
