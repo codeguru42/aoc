2447eb54bf5e794f6f878ec4934fe886e29f13b2
Code-Apprentice <codeguru42@gmail.com>
2019-12-02 19:18:25 -0700

    Stub for Day 2

---
+++ 2019/day02.py
@@ -0,0 +1,17 @@
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+def main():
+    with open('day02.txt') as file:
+        print(part1(file))
+    with open('day02.txt') as file:
+        print(part2(file))
+
+
+if __name__ == '__main__':
+    main()
