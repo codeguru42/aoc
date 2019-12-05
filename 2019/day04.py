9c0ea84e84e9e9d6196b1ed0f2edaec769ca393c
Code-Apprentice <codeguru42@gmail.com>
2019-12-04 21:35:05 -0700

    Stub for Day 4

---
+++ 2019/day04.py
@@ -0,0 +1,17 @@
+def part1(start, end):
+    pass
+
+
+def part2(start, end):
+    pass
+
+
+def main():
+    start = 246540
+    end = 787419
+    print(part1(start, end))
+    print(part2(start, end))
+
+
+if __name__ == '__main__':
+    main()
