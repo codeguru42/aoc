3ecf4356de2d28c27e6deac8a91685ebd7504ef7
Code-Apprentice <codeguru42@gmail.com>
2019-12-04 21:51:59 -0700

    AOC 2019, Day 4, Part 1

--- 2019/day04.py
+++ 2019/day04.py
@@ -1,5 +1,32 @@
+from itertools import tee
+
+
+def digits(n):
+    while n > 0:
+        yield n % 10
+        n //= 10
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
 def part1(start, end):
-    pass
+    count = 0
+    for n in range(start, end + 1):
+        has_double = False
+        monotonic_increasing = True
+        for a, b in pairwise(digits(n)):
+            if a == b:
+                has_double = True
+            if a < b:
+                monotonic_increasing = False
+        if has_double and monotonic_increasing:
+            count += 1
+    return count
 
 
 def part2(start, end):
