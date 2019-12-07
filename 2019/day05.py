a91f81dd9af16cf58c759d78b38c4808e25902ba
Code-Apprentice <codeguru42@gmail.com>
2019-12-07 11:31:49 -0700

    Fix how arguments are parsed

--- 2019/day05.py
+++ 2019/day05.py
@@ -1,5 +1,5 @@
 def part1(program):
-    pass
+    return run_program(list(program))
 
 
 def part2(program):
@@ -7,15 +7,25 @@ def part2(program):
 
 
 def digits(n):
-    while n > 0:
+    count = 0
+    while n > 0 or count < 3:
         yield n % 10
         n //= 10
+        count += 1
 
 
 def parse_inst(memory, inst_ptr):
     inst = memory[inst_ptr:inst_ptr+4]
     opcode = inst[0] % 100
-    args = [arg if mode == 1 else memory[arg] for arg, mode in zip(inst[1:], digits(opcode // 100))]
+    if opcode == 1 or opcode == 2:
+        args = [arg if mode == 1 else memory[arg] for arg, mode in zip(inst[1:], digits(inst[0] // 100))]
+        # Last argument is an lvalue
+        args[-1] = inst[-1]
+    elif opcode == 3:
+        args = [inst[1]]
+    elif opcode == 4:
+        mode = inst[0] // 100 % 10
+        args = [inst[1] if mode else memory[inst[1]]]
     return opcode, args
 
 
@@ -31,12 +41,13 @@ def run_program(memory):
             jump = 4
         elif opcode == 3:
             user_input = input()
-            memory[args[0]] = user_input
+            memory[args[0]] = int(user_input)
             jump = 2
         elif opcode == 4:
-            print(memory[args[0]])
+            print(args[0])
             jump = 2
         inst_ptr += jump
+        opcode, args = parse_inst(memory, inst_ptr)
 
 
 def main():
