ef5ea7aa4f33e6a6e6be756032e7b036d2f04341
Code-Apprentice <codeguru42@gmail.com>
2019-12-11 09:12:54 -0700

    AOC2019, Day 6: Solution to Part 1

--- 2019/day06.py
+++ 2019/day06.py
@@ -9,8 +9,14 @@ def parse_orbits(file):
     return orbits
 
 
+def count_orbits(planet, orbits, count):
+    if not orbits[planet]:
+        return count
+    return sum(count_orbits(p, orbits, count + 1) for p in orbits[planet]) + count
+
+
 def part1(orbits):
-    pass
+    return count_orbits('COM', orbits, 0)
 
 
 def part2(orbits):
@@ -21,8 +27,9 @@ def main():
     with open('day06.txt') as file:
         orbits = parse_orbits(file)
         print(orbits)
-    print(part1(file))
-    print(part2(file))
+        print(orbits['COM'])
+    print(part1(orbits))
+    print(part2(orbits))
 
 
 if __name__ == '__main__':
