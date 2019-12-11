028b4f819e2be4498fca54e65af9ea6b38322e33
Code-Apprentice <codeguru42@gmail.com>
2019-12-10 17:42:51 -0700

    AOC2019, Day 6: Stubs

---
+++ 2019/day06.py
@@ -0,0 +1,17 @@
+def part1(orbits):
+    pass
+
+
+def part2(orbits):
+    pass
+
+
+def main():
+    with open('day06.txt') as file:
+        print(part1(file))
+    with open('day06.txt') as file:
+        print(part2(file))
+
+
+if __name__ == '__main__':
+    main()
