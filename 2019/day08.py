f0be7fa05b3fe7441783b92fc0d140eda186c36d
Code-Apprentice <codeguru42@gmail.com>
2019-12-12 08:32:51 -0700

    AOC2019, Day 8: Stub

---
+++ 2019/day08.py
@@ -0,0 +1,21 @@
+def parse(file):
+    pass
+
+
+def part1(layers):
+    pass
+
+
+def part2(layers):
+    pass
+
+
+def main():
+    with open('day08.txt') as file:
+        layers = parse(file)
+        print(part1(layers))
+        print(part2(layers))
+
+
+if __name__ == '__main__':
+    main()
