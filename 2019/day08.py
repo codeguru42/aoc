78183400a33072e143b80a25c43b9df7d36e6c0f
Code-Apprentice <codeguru42@gmail.com>
2019-12-12 08:47:45 -0700

    AOC2019, Day 8: Solution to Part 1

--- 2019/day08.py
+++ 2019/day08.py
@@ -1,9 +1,28 @@
-def parse(file):
-    pass
+from collections import Counter
+from itertools import zip_longest
+from sys import maxsize
+
+
+def grouper(iterable, n, fillvalue=None):
+    """Collect data into fixed-length chunks or blocks"""
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def parse(file, size):
+    return grouper(file, size)
 
 
 def part1(layers):
-    pass
+    zeroes = maxsize
+    ones_and_twos = 0
+    for layer in layers:
+        counts = Counter(layer)
+        if counts['0'] < zeroes:
+            zeroes = counts['0']
+            ones_and_twos = counts['1'] * counts['2']
+    return ones_and_twos
 
 
 def part2(layers):
@@ -11,8 +30,9 @@ def part2(layers):
 
 
 def main():
+    layer_size = 25*6
     with open('day08.txt') as file:
-        layers = parse(file)
+        layers = parse(file.read().strip(), layer_size)
         print(part1(layers))
         print(part2(layers))
 
