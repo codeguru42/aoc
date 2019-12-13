bcf42a76cee69b582f91856ead7cf006156c6e42
Code-Apprentice <codeguru42@gmail.com>
2019-12-13 12:13:58 -0700

    AOC2019, Day 3: Stubs

---
+++ 2019/day03.py
@@ -0,0 +1,17 @@
+def part1(wires):
+    pass
+
+
+def part2(wires):
+    pass
+
+
+def main():
+    with open('day03.txt') as file:
+        wires = file.readlines()
+        print(part1(wires))
+        print(part2(wires))
+
+
+if __name__ == '__main__':
+    main()
