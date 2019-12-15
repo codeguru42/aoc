e18df5d3765e5a1b03fbbd0c93bbb24dfe15e9f7
Code-Apprentice <codeguru42@gmail.com>
2019-12-14 22:15:36 -0700

    AOC2019, Day 4: Tests for Part 2

--- 2019/day04.py
+++ 2019/day04.py
@@ -12,6 +12,15 @@ class Day04Test(unittest.TestCase):
     def test_part1_c(self):
         self.assertFalse(is_password1(123789))
 
+    def test_part2_a(self):
+        self.assertTrue(is_password2(112233))
+
+    def test_part2_b(self):
+        self.assertFalse(is_password2(123444))
+
+    def test_part2_c(self):
+        self.assertTrue(is_password2(111122))
+
 
 def digits(n):
     while n > 0:
