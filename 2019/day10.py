c76079d59a8dc481a8aa8e8094fc5c80901986d5
Code-Apprentice <codeguru42@gmail.com>
2019-12-16 20:01:35 -0700

    AOC2019, Day 10: Stubs

---
+++ 2019/day10.py
@@ -0,0 +1,17 @@
+def part1(asteroids):
+    pass
+
+
+def part2(asteroids):
+    pass
+
+
+def main():
+    with open('day10.txt') as file:
+        asteroids = file.readlines()
+        print(part1(asteroids))
+        print(part2(asteroids))
+
+
+if __name__ == '__main__':
+    main()
