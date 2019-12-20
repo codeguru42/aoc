e4f0c941be9b66d86de87bea0c439b900a303256
Code Apprentice <codeguru42@gmail.com>
2019-12-20 16:45:29 -0700

    AOC 2019, Day 7: Use coroutines for Part 1

--- 2019/day07.py
+++ 2019/day07.py
@@ -33,13 +33,15 @@ def part1(program, inp):
     max_phase = None
     phases = range(5)
     for phase in permutations(phases):
-        next_inp = inp
-        for p in phase:
-            inp_list = [p, next_inp]
-            outp = run_program(list(program), inp_list)
-            next_inp = outp[0]
-        if outp[0] > max_outp:
-            max_outp = outp[0]
+        amps = [(run_program(list(program)), p) for p in phase]
+        outp = inp
+        for amp, p in amps:
+            inp_list = [p, outp]
+            outp = next(amp)
+            for i in inp_list:
+                outp = amp.send(i)
+        if outp > max_outp:
+            max_outp = outp
             max_phase = phase
     return max_outp, max_phase
 
@@ -79,8 +81,7 @@ def parse_inst(memory, inst_ptr):
     return opcode, args
 
 
-def run_program(memory, inp):
-    outp = []
+def run_program(memory):
     inst_ptr = 0
     opcode, args = parse_inst(memory, inst_ptr)
     while opcode != 99:
@@ -91,10 +92,10 @@ def run_program(memory, inp):
             memory[args[2]] = args[0] * args[1]
             jump = 4
         elif opcode == 3:
-            memory[args[0]] = int(inp.pop(0))
+            memory[args[0]] = yield
             jump = 2
         elif opcode == 4:
-            outp.append(args[0])
+            yield args[0]
             jump = 2
         elif opcode == 5:
             if args[0] != 0:
@@ -118,7 +119,6 @@ def run_program(memory, inp):
             raise Exception(f'Invalid opcode {opcode} at address {inst_ptr}')
         inst_ptr += jump
         opcode, args = parse_inst(memory, inst_ptr)
-    return outp
 
 
 def main():
