78b1793e7fe3e494b4add65d36330753d2a7ac9f
Code-Apprentice <codeguru42@gmail.com>
2019-12-22 05:22:40 -0700

    AOC2019, Day 12: Stubs

---
+++ 2019/day12.py
@@ -0,0 +1,21 @@
+def parse_moons(file):
+    pass
+
+
+def part1(moons):
+    pass
+
+
+def part2(moons):
+    pass
+
+
+def main():
+    with open('day12.txt') as file:
+        moons = parse_moons(file)
+        print(part1(moons))
+        print(part2(moons))
+
+
+if __name__ == '__main__':
+    main()
