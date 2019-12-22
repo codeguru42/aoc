5044ce7f248d3754a651e4a208bc1e6cb85e3f9b
Code-Apprentice <codeguru42@gmail.com>
2019-12-22 05:26:55 -0700

    AOC2019, Day 12: Test case for parse_moons()

--- 2019/day12.py
+++ 2019/day12.py
@@ -1,3 +1,20 @@
+import unittest
+
+example_moons = '''
+<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>
+'''
+
+
+class TestDay12(unittest.TestCase):
+    def test_parse_moons(self):
+        moons = parse_moons(example_moons.split())
+        expected = [(1, 0, 2), (2, -10, -7), (4, -8, 8), (3, 5, -1)]
+        self.assertEqual(moons, expected)
+
+
 def parse_moons(file):
     pass
 
