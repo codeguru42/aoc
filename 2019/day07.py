d1016a10c0c302deccc0790ed3d6a699cb31a29e
Code-Apprentice <codeguru42@gmail.com>
2019-12-23 22:51:36 -0700

    AOC 2019, Day 7: Tests for Part 2

--- 2019/day07.py
+++ 2019/day07.py
@@ -27,6 +27,22 @@ class Day07Test(unittest.TestCase):
         result = part1(program, 0)
         self.assertEqual(expected, result)
 
+    def test_part2_a(self):
+        expected_outp = 139629729
+        expected_phase = (9, 8, 7, 6, 5)
+        expected = (expected_outp, expected_phase)
+        program = [3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5]
+        result = part2(program, 0)
+        self.assertEqual(expected, result)
+
+    def test_part2_b(self):
+        expected_outp = 18216
+        expected_phase = (9, 7, 8, 5, 6)
+        expected = (expected_outp, expected_phase)
+        program = [3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10]
+        result = part2(program, 0)
+        self.assertEqual(expected, result)
+
 
 def part1(program, inp):
     max_outp = 0
@@ -46,8 +62,22 @@ def part1(program, inp):
     return max_outp, max_phase
 
 
-def part2(program):
-    run_program(list(program))
+def part2(program, inp):
+    max_outp = 0
+    max_phase = None
+    phases = range(5)
+    for phase in permutations(phases):
+        amps = [(run_program(list(program)), p) for p in phase]
+        outp = inp
+        for amp, p in amps:
+            inp_list = [p, outp]
+            outp = next(amp)
+            for i in inp_list:
+                outp = amp.send(i)
+        if outp > max_outp:
+            max_outp = outp
+            max_phase = phase
+    return max_outp, max_phase
 
 
 def digits(n):
@@ -125,7 +155,7 @@ def main():
     with open('day07.txt') as file:
         int_codes = [int(x) for x in file.readline().split(',')]
         print(part1(int_codes, 0))
-        print(part2(int_codes))
+        print(part2(int_codes, 0))
 
 
 if __name__ == '__main__':
