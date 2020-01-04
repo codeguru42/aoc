c9fa5ccf3567088bb4e677f436df7ca513467b28
Code-Apprentice <codeguru42@gmail.com>
2020-01-04 10:11:49 -0700

    AOC 2019, Day 14: Stubs

---
+++ 2019/day14.py
@@ -0,0 +1,21 @@
+def parse_reactions(file):
+    pass
+
+
+def part1(reactions):
+    pass
+
+
+def part2(reactions):
+    pass
+
+
+def main():
+    with open('day14.txt') as file:
+        reactions = parse_reactions(file)
+        print(part1(reactions))
+        print(part2(reactions))
+
+
+if __name__ == '__main__':
+    main()
