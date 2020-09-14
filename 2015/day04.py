fc646cbdbb1a1a9c3050835b92b587f877b763e1
Code-Apprentice <codeguru42@gmail.com>
2020-09-14 01:00:55 -0600

    AOC 2015, Day 4: Add unit tests

--- 2015/day04.py
+++ 2015/day04.py
@@ -1,15 +1,28 @@
-def part1():
+import unittest
+
+
+class TestPart1(unittest.TestCase):
+    def test_example1(self):
+        self.assertEqual(609043, part1('abcdef'))
+
+    def test_example2(self):
+        self.assertEqual(1048970, part1('pqrstuv'))
+
+
+def part1(secret_key):
     pass
 
 
-def part2():
+def part2(secret_key):
     pass
 
 
 def main():
-    print(part1())
-    print(part2())
+    secret_key = 'bgvyzdsv'
+    print(part1(secret_key))
+    print(part2(secret_key))
 
 
 if __name__ == '__main__':
+    unittest.main()
     main()
