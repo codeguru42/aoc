b7d7bc9a80a54bd7e91c5496f2cc3ab9e3cd3abb
Code-Apprentice <codeguru42@gmail.com>
2020-09-13 22:47:09 -0600

    AOC 20015, Day 1, Part 2

--- 2015/day01.py
+++ 2015/day01.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def main():
+def part1():
     file = sys.argv[1]
     floor = 0
     with open(file) as fp:
@@ -12,6 +12,28 @@ def main():
                 elif c == ')':
                     floor -= 1
     print(floor)
+
+
+def part2():
+    file = sys.argv[1]
+    floor = 0
+    pos = 1
+    with open(file) as fp:
+        for line in fp:
+            for c in line:
+                if c == '(':
+                    floor += 1
+                elif c == ')':
+                    floor -= 1
+                if floor < 0:
+                    print(pos)
+                    return
+                pos += 1
+
+
+def main():
+    part1()
+    part2()
 
 
 if __name__ == '__main__':
