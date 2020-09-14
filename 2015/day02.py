c192e1116f0c9174b0624e3909544d8350562948
Code-Apprentice <codeguru42@gmail.com>
2020-09-13 23:01:54 -0600

    AOC 2015, Day 2: Solution for Part 1

--- 2015/day02.py
+++ 2015/day02.py
@@ -1,5 +1,28 @@
+import sys
+
+
+def area(l, w):
+    return l * w
+
+
+def min_area(l, w, h):
+    return min(area(x, y) for x, y in ((l, w), (l, h), (w, h)))
+
+
+def surface_area(l, w, h):
+    return 2*l*w + 2*l*h + 2*w*h
+
+
+def total_wrapping_paper(lines):
+    for line in lines:
+        dims = [int(d) for d in line.split('x')]
+        yield surface_area(*dims) + min_area(*dims)
+
+
 def part1():
-    pass
+    filename = sys.argv[1]
+    with open(filename) as file:
+        print(sum(total_wrapping_paper(file)))
 
 
 def part2():
