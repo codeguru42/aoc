44578727304050c818c24b15a82648fd4254741d
Code-Apprentice <codeguru42@gmail.com>
2020-09-13 23:30:05 -0600

    AOC 2015, Day 3: Solution for Part 1

--- 2015/day03.py
+++ 2015/day03.py
@@ -1,12 +1,26 @@
 import sys
 
 
+def houses(directions):
+    x, y = 0, 0
+    yield x, y
+    for d in directions:
+        if d == '>':
+            x += 1
+        elif d == '<':
+            x -= 1
+        elif d == '^':
+            y += 1
+        elif d == 'v':
+            y -= 1
+        yield x, y
+
+
 def part1():
     filename = sys.argv[1]
     with open(filename) as file:
         for line in file:
-            for c in line:
-                print(c)
+            print(len(set(houses(line))))
 
 
 def part2():
