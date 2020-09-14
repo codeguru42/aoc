b94a5626f4c6f8578d45e7a53d59aa201f901650
Code-Apprentice <codeguru42@gmail.com>
2020-09-13 23:08:36 -0600

    AOC 2015, Day 2: Solution for Part 2

--- 2015/day02.py
+++ 2015/day02.py
@@ -9,14 +9,32 @@ def min_area(l, w, h):
     return min(area(x, y) for x, y in ((l, w), (l, h), (w, h)))
 
 
+def perimeter(l, w):
+    return 2*l + 2 * w
+
+
+def min_perimeter(l, w, h):
+    return min(perimeter(x, y) for x, y in ((l, w), (l, h), (w, h)))
+
+
 def surface_area(l, w, h):
     return 2*l*w + 2*l*h + 2*w*h
+
+
+def volume(l, w, h):
+    return l*w*h
 
 
 def total_wrapping_paper(lines):
     for line in lines:
         dims = [int(d) for d in line.split('x')]
         yield surface_area(*dims) + min_area(*dims)
+
+
+def total_ribbon(lines):
+    for line in lines:
+        dims = [int(d) for d in line.split('x')]
+        yield min_perimeter(*dims) + volume(*dims)
 
 
 def part1():
@@ -26,7 +44,9 @@ def part1():
 
 
 def part2():
-    pass
+    filename = sys.argv[1]
+    with open(filename) as file:
+        print(sum(total_ribbon(file)))
 
 
 def main():
