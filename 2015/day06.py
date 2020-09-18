77ebefa3f67471111375a48602145321c90a7ac5
Code-Apprentice <codeguru42@gmail.com>
2020-09-17 21:18:43 -0600

    AOC 2015, Day 6: Finish solution for Part 1

--- 2015/day06.py
+++ 2015/day06.py
@@ -5,7 +5,31 @@ def parse(line):
     inst_regex = r'(\D*)(\d*),(\d*) through (\d*),(\d*)'
     matches = re.fullmatch(inst_regex, line.strip())
     groups = matches.groups()
-    return groups[0], (int(groups[1]), int(groups[2])), (int(groups[3]), int(groups[4]))
+    return groups[0].strip(), (int(groups[1]), int(groups[2])), (int(groups[3]), int(groups[4]))
+
+
+def turn_on(lights, start, end):
+    startx, starty = start
+    endx, endy = end
+    for i in range(startx, endx+1):
+        for j in range(starty, endy+1):
+            lights[i][j] = True
+
+
+def turn_off(lights, start, end):
+    startx, starty = start
+    endx, endy = end
+    for i in range(startx, endx+1):
+        for j in range(starty, endy+1):
+            lights[i][j] = False
+
+
+def toggle(lights, start, end):
+    startx, starty = start
+    endx, endy = end
+    for i in range(startx, endx+1):
+        for j in range(starty, endy+1):
+            lights[i][j] = not lights[i][j]
 
 
 def part1():
@@ -13,7 +37,13 @@ def part1():
     with open('day06.txt') as file:
         for line in file:
             inst, start, end = parse(line)
-            print(inst, start, end)
+            if inst == 'turn on':
+                turn_on(lights, start, end)
+            elif inst == 'turn off':
+                turn_off(lights, start, end)
+            elif inst == 'toggle':
+                toggle(lights, start, end)
+    return sum(sum(row) for row in lights)
 
 
 def part2():
