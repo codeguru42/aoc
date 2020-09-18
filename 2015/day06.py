f4f372ca845b31ad6110d4aff178790e814d3538
Code-Apprentice <codeguru42@gmail.com>
2020-09-17 21:08:28 -0600

    AOC 2015, Day 6: Parse input

--- 2015/day06.py
+++ 2015/day06.py
@@ -1,5 +1,19 @@
+import re
+
+
+def parse(line):
+    inst_regex = r'(\D*)(\d*),(\d*) through (\d*),(\d*)'
+    matches = re.fullmatch(inst_regex, line.strip())
+    groups = matches.groups()
+    return groups[0], (int(groups[1]), int(groups[2])), (int(groups[3]), int(groups[4]))
+
+
 def part1():
-    pass
+    lights = [[False] * 1000 for i in range(1000)]
+    with open('day06.txt') as file:
+        for line in file:
+            inst, start, end = parse(line)
+            print(inst, start, end)
 
 
 def part2():
