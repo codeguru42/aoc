355ceeef96c5da4499b671f12253838881a9c9ed
Code-Apprentice <codeguru42@gmail.com>
2020-12-04 15:58:56 -0700

    AOC 2020, Day 1: Solve Part 1

--- 2020/day01.py
+++ 2020/day01.py
@@ -1,9 +1,18 @@
 def part1():
-    pass
+    for expense1 in read_expenses():
+        for expense2 in read_expenses():
+            if expense1 + expense2 == 2020:
+                return expense1 * expense2
 
 
 def part2():
     pass
+
+
+def read_expenses():
+    with open('day01.txt') as file:
+        for line in file:
+            yield int(line.strip())
 
 
 def main():
