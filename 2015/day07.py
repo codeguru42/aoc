38205e022a5e197ab1fd38da25eed82d7fb34507
Code-Apprentice <codeguru42@gmail.com>
2020-12-04 15:50:05 -0700

    AOC 2015, Day 7: Attempt to solve part 1

--- 2015/day07.py
+++ 2015/day07.py
@@ -1,12 +1,53 @@
+import unittest
+
+example1 = '''123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i'''
+
+
+class TestRun(unittest.TestCase):
+    def test_example1(self):
+        expected = {
+            'd': 72,
+            'e': 507,
+            'f': 492,
+            'g': 114,
+            'h': 65412,
+            'i': 65079,
+            'x': 123,
+            'y': 456,
+        }
+        wires = parse(example1.split('\n'))
+        actual = {wire: run(wires, wire) for wire in expected}
+        self.assertEqual(expected, actual)
+
+
 def parse(lines):
     parsed = [line.strip().split('->') for line in lines]
     wires = {wire.strip(): op.strip().split() for op, wire in parsed}
     return wires
 
 
+def run(wires, out_wire):
+    values = evaluate({}, out_wire, wires[out_wire])
+    return values[out_wire]
+
+
+def evaluate(values, wire, expr):
+    if len(expr) == 1:
+        values[wire] = expr[0]
+        return values
+
+
 def part1():
     with open('day07.txt') as file:
-        print(parse(file))
+        wires = parse(file)
+        return run(wires, 'a')
 
 
 def part2():
@@ -19,4 +60,5 @@ def main():
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
