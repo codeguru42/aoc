e8c0eee3a4325cf07fbf32673d47b4891b8241db
Code-Apprentice <codeguru42@gmail.com>
2020-12-04 19:09:15 -0700

    AOC 2020, Day 2: Solve Part 2 with tests

--- 2020/day02.py
+++ 2020/day02.py
@@ -1,4 +1,16 @@
 import collections
+import unittest
+
+
+class Part2Test(unittest.TestCase):
+    def test_example1(self):
+        self.assertTrue(validate2('1-3 a: abcde'))
+
+    def test_example2(self):
+        self.assertFalse(validate2('1-3 b: cdefg'))
+
+    def test_example3(self):
+        self.assertFalse(validate2('2-9 c: ccccccccc'))
 
 
 def part1(lines):
@@ -15,15 +27,31 @@ def part1(lines):
     return valid_count
 
 
-def part2():
-    pass
+def validate2(line):
+    parts = line.split()
+    index1, index2 = parts[0].split('-')
+    index1 = int(index1) - 1
+    index2 = int(index2) - 1
+    letter = parts[1][0]
+    password = parts[2]
+    return (password[index1] == letter) != (password[index2] == letter)
+
+
+def part2(lines):
+    valid_count = 0
+    for line in lines:
+        if validate2(line):
+            valid_count += 1
+    return valid_count
 
 
 def main():
     with open('day02.txt') as lines:
         print(part1(lines))
-    print(part2())
+    with open('day02.txt') as lines:
+        print(part2(lines))
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
