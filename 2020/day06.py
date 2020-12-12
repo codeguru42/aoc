abb1ad7e2966a444b680fc970e16f82c8115c16c
Code-Apprentice <codeguru42@gmail.com>
2020-12-11 21:29:32 -0700

    AOC 2020, Day 6: Solve Part 1

--- 2020/day06.py
+++ 2020/day06.py
@@ -1,5 +1,48 @@
-def part1():
-    pass
+import unittest
+
+example = """abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"""
+
+
+class TestParse(unittest.TestCase):
+    def test_example(self):
+        expected = [
+            set('abc'),
+            set('abc'),
+            set('abc'),
+            set('a'),
+            set('b'),
+        ]
+        actual = parse_yeses(example.splitlines())
+        self.assertEqual(expected, list(actual))
+
+
+def parse_yeses(file):
+    group = set()
+    for line in file:
+        if line.strip():
+            group |= set(line.strip())
+        else:
+            yield group
+            group = set()
+    yield group
+
+
+def part1(file):
+    return sum(len(s) for s in parse_yeses(file))
 
 
 def part2():
@@ -7,9 +50,11 @@ def part2():
 
 
 def main():
-    print(part1())
+    with open('day06.txt') as file:
+        print(part1(file))
     print(part2())
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
