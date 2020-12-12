23d1c7254ec1f11d8b7047dd97179dd5bf3f65da
Code-Apprentice <codeguru42@gmail.com>
2020-12-12 12:02:09 -0700

    AOC 2020, Day 7: Parse input

--- 2020/day07.py
+++ 2020/day07.py
@@ -1,3 +1,21 @@
+import re
+
+
+def parse(file):
+    no_bags = r'([a-z ]+) bags contain no other bags.'
+    contains_bags = r'([a-z ]+) bags contain (\d+) ([a-z ]+) bags?(, (\d+) ([a-z ]+) bags?)*\.'
+    bags = {}
+    for line in file:
+        no_bags_matches = re.fullmatch(no_bags, line.strip())
+        if no_bags_matches:
+            bags[no_bags_matches.group(1)] = None
+        else:
+            contains_bags_matches = re.fullmatch(contains_bags, line.strip())
+            bag_groups = contains_bags_matches.groups()
+            bags[bag_groups[0]] = bag_groups[2::3]
+    return bags
+
+
 def part1():
     pass
 
@@ -7,6 +25,9 @@ def part2():
 
 
 def main():
+    with open('day07.txt') as file:
+        bags = parse(file)
+    print(bags)
     print(part1())
     print(part2())
 
