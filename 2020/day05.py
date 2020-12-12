be12f34d4af3d023aa78f873d983419c163badd4
Code-Apprentice <codeguru42@gmail.com>
2020-12-11 17:02:01 -0700

    AOC 2020, Day 5: Implement binary_search()

--- 2020/day05.py
+++ 2020/day05.py
@@ -1,3 +1,46 @@
+import re
+import unittest
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test_example1(self):
+        self.assertEqual((0, 63), binary_search('F', 0, 127))
+
+    def test_example2(self):
+        self.assertEqual((32, 63), binary_search('B', 0, 63))
+
+    def test_example3(self):
+        self.assertEqual((32, 47), binary_search('F', 32, 63))
+
+    def test_example4(self):
+        self.assertEqual((40, 47), binary_search('B', 32, 47))
+
+    def test_example5(self):
+        self.assertEqual((44, 47), binary_search('B', 40, 47))
+
+    def test_example6(self):
+        self.assertEqual((44, 45), binary_search('F', 44, 47))
+
+    def test_example7(self):
+        self.assertEqual((44, 44), binary_search('F', 44, 45))
+
+    def test_example8(self):
+        self.assertEqual((4, 7), binary_search('R', 0, 7))
+
+    def test_example9(self):
+        self.assertEqual((4, 5), binary_search('L', 4, 7))
+
+    def test_example10(self):
+        self.assertEqual((5, 5), binary_search('R', 4, 5))
+
+
+def binary_search(next, min, max):
+    mid = (min + max) // 2
+    if next in ('F', 'L'):
+        return min, mid
+    return mid + 1, max
+
+
 def part1():
     pass
 
