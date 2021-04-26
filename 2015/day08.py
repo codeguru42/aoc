9167960e382780c14db5a446eb92355ad6c28db2
Code-Apprentice <codeguru42@gmail.com>
2021-04-25 21:34:24 -0600

    AOC 2015, Day 8: Solve Part 2

--- 2015/day08.py
+++ 2015/day08.py
@@ -9,7 +9,18 @@ def part1():
 
 
 def part2():
-    pass
+    replace = {
+        ord('"'): '\\"',
+        ord('\\'): '\\\\'
+    }
+    text_sum = 0
+    repr_sum = 0
+    with open('day08.txt') as file:
+        for line in file:
+            text_sum += len(line.strip())
+            repr_line = line.strip().translate(replace)
+            repr_sum += len(repr_line) + 2
+    return repr_sum - text_sum
 
 
 def main():
