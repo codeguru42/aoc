deff15b476ac1f1c5d34f07361afec45465ce306
Code-Apprentice <codeguru42@gmail.com>
2021-11-07 15:42:49 -0700

    AOC 2015, Day 10: look_and_say() helper function

--- 2015/day10.py
+++ 2015/day10.py
@@ -1,3 +1,38 @@
+import unittest
+
+
+class LookAndSayTest(unittest.TestCase):
+    def test1(self):
+        self.assertEqual('11', look_and_say('1'))
+
+    def test2(self):
+        self.assertEqual('21', look_and_say('11'))
+
+    def test3(self):
+        self.assertEqual('1211', look_and_say('21'))
+
+    def test4(self):
+        self.assertEqual('111221', look_and_say('1211'))
+
+    def test5(self):
+        self.assertEqual('312211', look_and_say('111221'))
+
+
+def look_and_say(digits):
+    previous = digits[0]
+    count = 1
+    result = ''
+    for digit in digits[1:]:
+        if digit == previous:
+            count += 1
+        else:
+            result += str(count) + previous
+            count = 1
+        previous = digit
+    result += str(count) + previous
+    return result
+
+
 def part1():
     pass
 
@@ -12,4 +47,5 @@ def main():
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
