f6301e1cc23dc1994d92b1ca258d148b42ffe325
Code-Apprentice <codeguru42@gmail.com>
2021-11-06 22:55:04 -0600

    AOC 2015, Day 9: Parse input file

--- 2015/day09.py
+++ 2015/day09.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def part1():
     pass
 
@@ -6,9 +9,22 @@ def part2():
     pass
 
 
+def parse(file):
+    graph = defaultdict(defaultdict)
+    for line in file:
+        e = line.strip().split(' = ')
+        v = e[0].split(' to ')
+        graph[v[0]][v[1]] = int(e[1])
+        graph[v[1]][v[0]] = int(e[1])
+    return graph
+
+
 def main():
-    print(part1())
-    print(part2())
+    with open('day09.txt') as file:
+        graph = parse(file)
+        print(graph)
+        print(part1())
+        print(part2())
 
 
 if __name__ == '__main__':
