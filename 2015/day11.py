42891e2c92881d965b2fc36d9f94af13778d09a2
Code-Apprentice <codeguru42@gmail.com>
2021-11-07 16:02:55 -0700

    AOC 2015, Day 11: Helper functions with first example

--- 2015/day11.py
+++ 2015/day11.py
@@ -1,3 +1,31 @@
+import itertools
+import unittest
+
+
+class Day11Tests(unittest.TestCase):
+    def testHasStraight(self):
+        self.assertTrue(has_straight('hijklmmn'))
+
+    def testDoesNotContainIllegalLetters(self):
+        self.assertFalse(contains_illegal_letters('hijklmmn'))
+
+
+def has_straight(password):
+    count = 1
+    for prev, c in itertools.pairwise(password):
+        if chr(ord(prev) + 1) == c:
+            count += 1
+            if count == 3:
+                return True
+        else:
+            count = 0
+    return False
+
+
+def contains_illegal_letters(password):
+    return 'i' not in password and 'o' not in password and 'l' not in password
+
+
 def part1():
     pass
 
@@ -12,4 +40,5 @@ def main():
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
