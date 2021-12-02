46bea9720fc6505b03798c88df3ee0a215816fec
Code-Apprentice <codeguru42@gmail.com>
2021-12-01 19:02:54 -0700

    AOC 2021, Day 1: Solve Part 2

--- 2021/day01.py
+++ 2021/day01.py
@@ -18,13 +18,19 @@ def diffs(nums):
         yield y - x
 
 
+def three_sums(nums):
+    for threes in sliding_window(nums, 3):
+        yield sum(threes)
+
+
 def part1():
     with open('day01.txt') as file:
         return sum(diff > 0 for diff in diffs(int(line.strip()) for line in file))
 
 
 def part2():
-    pass
+    with open('day01.txt') as file:
+        return sum(diff > 0 for diff in diffs(three_sums(int(line.strip()) for line in file)))
 
 
 def main():
