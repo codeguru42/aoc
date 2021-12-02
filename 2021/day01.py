8d898a9e4fbabfd549d74be53f5c69d4e159a13b
Code-Apprentice <codeguru42@gmail.com>
2021-12-01 18:45:00 -0700

    AOC 2021, Day 1: Solve Part 1

--- 2021/day01.py
+++ 2021/day01.py
@@ -1,5 +1,26 @@
+import collections
+import itertools
+
+
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(itertools.islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+def diffs(nums):
+    for x, y in sliding_window(nums, 2):
+        yield y - x
+
+
 def part1():
-    pass
+    with open('day01.txt') as file:
+        return sum(diff > 0 for diff in diffs(int(line.strip()) for line in file))
 
 
 def part2():
