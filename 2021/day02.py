2f8bcbba4ea8dae28a7a8d6bdde471cf34a54562
Code-Apprentice <codeguru42@gmail.com>
2021-12-01 22:08:03 -0700

    AOC 2021, Day 2: Solve Part 2

--- 2021/day02.py
+++ 2021/day02.py
@@ -13,7 +13,18 @@ def part1():
 
 
 def part2():
-    pass
+    with open('day02.txt') as file:
+        x, y, aim = 0, 0, 0
+        for line in file:
+            ins = line.strip().split()
+            if ins[0] == 'forward':
+                x += int(ins[1])
+                y += aim * int(ins[1])
+            elif ins[0] == 'down':
+                aim += int(ins[1])
+            elif ins[0] == 'up':
+                aim -= int(ins[1])
+    return x * y
 
 
 def main():
