3a28660f8fc01b3fca0299985a597df40ba52253
Code-Apprentice <codeguru42@gmail.com>
2021-12-02 22:39:10 -0700

    AOC 2021, Day 3: Calculate oxygen generator rating

--- 2021/day03.py
+++ 2021/day03.py
@@ -1,4 +1,25 @@
+import unittest
 from collections import Counter
+
+example = '''
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010
+'''
+
+
+class Day3Test(unittest.TestCase):
+    def test_get_o2_rating(self):
+        self.assertEqual(23, get_o2_rating(l.strip() for l in example.strip().split()))
 
 
 def part1():
@@ -8,6 +29,20 @@ def part1():
         gamma = ''.join(c.most_common(1)[0][0] for c in counts)
         epsilon = ''.join('0' if c == '1' else '1' for c in gamma)
         return int(gamma, 2) * int(epsilon, 2)
+
+
+def get_o2_rating(bin_numbers):
+    result = list(bin_numbers)
+
+    for i in range(len(result[0])):
+        if len(result) == 1:
+            return int(result[0], 2)
+        bits = zip(*result)
+        counts = [Counter(b) for b in bits]
+        c = counts[i]
+        most = c.most_common(1)[0][0] if c['0'] != c['1'] else '1'
+        result = [x for x in result if x[i] == most]
+    return int(result[0], 2)
 
 
 def part2():
@@ -20,4 +55,5 @@ def main():
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
