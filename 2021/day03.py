20bb968c56d6ee44e990d91d1c2989be2737b87e
Code-Apprentice <codeguru42@gmail.com>
2021-12-02 22:13:27 -0700

    AOC 2021, Day 3: Solve Part 1

--- 2021/day03.py
+++ 2021/day03.py
@@ -1,5 +1,13 @@
+from collections import Counter
+
+
 def part1():
-    pass
+    with open('day03.txt') as file:
+        bits = zip(*[line.strip() for line in file])
+        counts = [Counter(b) for b in bits]
+        gamma = ''.join(c.most_common(1)[0][0] for c in counts)
+        epsilon = ''.join('0' if c == '1' else '1' for c in gamma)
+        return int(gamma, 2) * int(epsilon, 2)
 
 
 def part2():
