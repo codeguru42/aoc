0ce156e57b79b38ff78ff16823012174116f975b
Code-Apprentice <codeguru42@gmail.com>
2021-12-02 22:41:29 -0700

    AOC 2021, Day 3: Calculate CO2 scrubber rating

--- 2021/day03.py
+++ 2021/day03.py
@@ -21,6 +21,9 @@ class Day3Test(unittest.TestCase):
     def test_get_o2_rating(self):
         self.assertEqual(23, get_o2_rating(l.strip() for l in example.strip().split()))
 
+    def test_get_co2_rating(self):
+        self.assertEqual(10, get_co2_rating(l.strip() for l in example.strip().split()))
+
 
 def part1():
     with open('day03.txt') as file:
@@ -42,6 +45,21 @@ def get_o2_rating(bin_numbers):
         c = counts[i]
         most = c.most_common(1)[0][0] if c['0'] != c['1'] else '1'
         result = [x for x in result if x[i] == most]
+    return int(result[0], 2)
+
+
+def get_co2_rating(bin_numbers):
+    result = list(bin_numbers)
+
+    for i in range(len(result[0])):
+        if len(result) == 1:
+            return int(result[0], 2)
+        bits = zip(*result)
+        counts = [Counter(b) for b in bits]
+        c = counts[i]
+        most = c.most_common(1)[0][0] if c['0'] != c['1'] else '1'
+        least = '0' if most == '1' else '1'
+        result = [x for x in result if x[i] == least]
     return int(result[0], 2)
 
 
