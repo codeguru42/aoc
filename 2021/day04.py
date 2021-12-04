9a6cecd5e33fb76437f9212e3632dba448b32344
Code-Apprentice <codeguru42@gmail.com>
2021-12-03 23:43:22 -0700

    AOC 2021, Day 4: Parse input

--- 2021/day04.py
+++ 2021/day04.py
@@ -1,3 +1,27 @@
+class Bingo:
+    def __init__(self, board):
+        self.board = board
+
+    def __repr__(self):
+        return str(self.board)
+
+
+def parse_bingo(file):
+    for i in range(5):
+        yield file.readline().split()
+
+
+def parse_bingos(file):
+    while file.readline():
+        yield Bingo(list(parse_bingo(file)))
+
+
+def parse(file):
+    numbers = file.readline().split(',')
+    bingo = list(parse_bingos(file))
+    return numbers, bingo
+
+
 def part1():
     pass
 
@@ -7,6 +31,11 @@ def part2():
 
 
 def main():
+    with open('day04.txt') as file:
+        numbers, bingo = parse(file)
+
+    print(numbers)
+    print(bingo)
     print(part1())
     print(part2())
 
