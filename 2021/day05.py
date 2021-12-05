f95b66a36d4bd9c8abf29042031e374160c7e7fa
Code-Apprentice <codeguru42@gmail.com>
2021-12-05 14:52:10 -0700

    AOC 2021, Day 5: Solve Part 2

--- 2021/day05.py
+++ 2021/day05.py
@@ -18,15 +18,35 @@ def part1(lines):
     return sum(sum(c > 1 for c in row) for row in grid)
 
 
-def part2():
-    pass
+def part2(lines):
+    grid = [[0] * 1000 for _ in range(1000)]
+    for p1, p2 in lines:
+        x1, y1 = p1
+        x2, y2 = p2
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2) + 1):
+                grid[x1][y] += 1
+        elif y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2) + 1):
+                grid[x][y1] += 1
+        else:
+            x_min = min(x1, x2)
+            x_max = max(x1, x2)
+            xs = range(x_min, x_max + 1)
+            y_min = y1 if x_min == x1 else y2
+            y_max = y1 if x_max == x1 else y2
+            slope = (x_max - x_min) // (y_max - y_min)
+            ys = range(y_min, y_max + slope, slope)
+            for x, y in zip(xs, ys):
+                grid[x][y] += 1
+    return sum(sum(c > 1 for c in row) for row in grid)
 
 
 def main():
     with open('day05.txt') as file:
         lines = list(parse(file))
     print(part1(lines))
-    print(part2())
+    print(part2(lines))
 
 
 if __name__ == '__main__':
