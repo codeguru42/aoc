7f170bfd6e382308edfcee7a1e2106185bfabb84
Code-Apprentice <codeguru42@gmail.com>
2021-12-05 22:13:39 -0700

    AOC 2021, Day 6: Solve Part 1

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,5 +1,35 @@
-def part1():
-    pass
+import unittest
+
+
+class TestDay06(unittest.TestCase):
+    def test_next_day1(self):
+        ages = [3, 4, 3, 1, 2]
+        expected = [2, 3, 2, 0, 1]
+        self.assertEqual(expected, list(next_day(ages)))
+
+    def test_next_day2(self):
+        ages = [2, 3, 2, 0, 1]
+        expected = [1, 2, 1, 6, 0, 8]
+        self.assertEqual(expected, list(next_day(ages)))
+
+
+def next_day(ages):
+    new_fish = 0
+    for age in ages:
+        new_age = age - 1
+        if new_age == -1:
+            new_fish += 1
+            yield 6
+        else:
+            yield new_age
+
+    yield from [8] * new_fish
+
+
+def part1(ages):
+    for _ in range(80):
+        ages = next_day(ages)
+    return len(list(ages))
 
 
 def part2():
@@ -7,9 +37,12 @@ def part2():
 
 
 def main():
-    print(part1())
+    with open('day06.txt') as file:
+        ages = [int(a) for a in file.readline().strip().split(',')]
+    print(part1(ages))
     print(part2())
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
