0175affbfdc959a0d966038d7d8f2e3a2519bb61
Code-Apprentice <codeguru42@gmail.com>
2021-12-06 22:12:43 -0700

    AOC 2021, Day 7: Solve Part 2

--- 2021/day07.py
+++ 2021/day07.py
@@ -1,20 +1,28 @@
-def calculate_fuel(positions, new_pos):
+def calculate_total_fuel(positions, new_pos):
     return sum(abs(x - new_pos) for x in positions)
 
 
 def part1(positions):
-    return min(calculate_fuel(positions, i) for i in range(max(positions)))
+    return min(calculate_total_fuel(positions, i) for i in range(max(positions)))
 
 
-def part2():
-    pass
+def calculate_total_fuel2(positions, new_pos):
+    return sum(calculate_fuel(abs(x - new_pos)) for x in positions)
+
+
+def calculate_fuel(steps):
+    return steps * (steps + 1) / 2
+
+
+def part2(positions):
+    return min(calculate_total_fuel2(positions, i) for i in range(max(positions)))
 
 
 def main():
     with open('day07.txt') as file:
         positions = [int(x) for x in file.readline().strip().split(',')]
     print(part1(positions))
-    print(part2())
+    print(part2(positions))
 
 
 if __name__ == '__main__':
