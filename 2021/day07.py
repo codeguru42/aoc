2ee2f8f3bfd1ae139ca16a61730f4c5bb6c4e8ba
Code-Apprentice <codeguru42@gmail.com>
2021-12-06 22:07:48 -0700

    AOC 2021, Day 7: Solve Part 1

--- 2021/day07.py
+++ 2021/day07.py
@@ -1,5 +1,9 @@
-def part1():
-    pass
+def calculate_fuel(positions, new_pos):
+    return sum(abs(x - new_pos) for x in positions)
+
+
+def part1(positions):
+    return min(calculate_fuel(positions, i) for i in range(max(positions)))
 
 
 def part2():
@@ -7,7 +11,9 @@ def part2():
 
 
 def main():
-    print(part1())
+    with open('day07.txt') as file:
+        positions = [int(x) for x in file.readline().strip().split(',')]
+    print(part1(positions))
     print(part2())
 
 
