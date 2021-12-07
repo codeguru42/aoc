af2e9cdeffc32b2b75005ae05f768149d682257b
Code-Apprentice <codeguru42@gmail.com>
2021-12-06 17:18:52 -0700

    AOC 2021, Day 6: Rethink solution for part 1

--- 2021/day06.py
+++ 2021/day06.py
@@ -1,35 +1,41 @@
 import unittest
+from collections import Counter
 
 
 class TestDay06(unittest.TestCase):
     def test_next_day1(self):
         ages = [3, 4, 3, 1, 2]
         expected = [2, 3, 2, 0, 1]
-        self.assertEqual(expected, list(next_day(ages)))
+        self.assertEqual(Counter(expected), next_day(Counter(ages)))
 
     def test_next_day2(self):
         ages = [2, 3, 2, 0, 1]
         expected = [1, 2, 1, 6, 0, 8]
-        self.assertEqual(expected, list(next_day(ages)))
+        self.assertEqual(Counter(expected), next_day(Counter(ages)))
+
+    def test_part1(self):
+        with open('day06.txt') as file:
+            ages = [int(a) for a in file.readline().strip().split(',')]
+            self.assertEqual(365862, part1(ages))
 
 
 def next_day(ages):
-    new_fish = 0
+    new_ages = Counter()
     for age in ages:
         new_age = age - 1
         if new_age == -1:
-            new_fish += 1
-            yield 6
+            new_ages[6] += ages[age]
+            new_ages[8] += ages[age]
         else:
-            yield new_age
-
-    yield from [8] * new_fish
+            new_ages[new_age] += ages[age]
+    return new_ages
 
 
 def spawn_fish(ages, days):
+    counts = Counter(ages)
     for _ in range(days):
-        ages = next_day(ages)
-    return len(list(ages))
+        counts = next_day(counts)
+    return sum(counts.values())
 
 
 def part1(ages):
