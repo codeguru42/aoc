05a291f505d5e072e23e9929c7123d449f53663f
Code-Apprentice <codeguru42@gmail.com>
2021-12-08 00:25:21 -0700

    AOC 2021, Day 8: Comments describing solution

--- 2021/day08.py
+++ 2021/day08.py
@@ -44,14 +44,21 @@ def build_map(signal: typing.Iterable[str]):
         8: set(length_map[7][0]),
     }
 
+    # 3 has five segments and contains the same segments as in a 1
     seven_segment_map[3] = [set(x) for x in length_map[5] if seven_segment_map[1] < set(x)][0]
+    # 5 has five segments and contains segments that are in 4 but not in 1
     seven_segment_map[5] = [set(x) for x in length_map[5] if (seven_segment_map[4] - seven_segment_map[1]) < set(x)][0]
+    # 6 has six segments and does not contain all segments in 1
     seven_segment_map[6] = [set(x) for x in length_map[6] if not seven_segment_map[1] < set(x)][0]
+    # 9 has six segments and contains all segments in 4
     seven_segment_map[9] = [set(x) for x in length_map[6] if seven_segment_map[4] < set(x)][0]
 
+    # 0 has 6 segments and isn't 6 or 9
     seven_segment_map[0] = [set(x) for x in length_map[6] if set(x) not in [seven_segment_map[i] for i in (6, 9)]][0]
+    # 2 has 5 segments and isn't 3 or 5
     seven_segment_map[2] = [set(x) for x in length_map[5] if set(x) not in [seven_segment_map[i] for i in (3, 5)]][0]
 
+    # invert mapping but key is a string with characters in alphabetical order
     return {''.join(sorted(value)): key for key, value in seven_segment_map.items()}
 
 
