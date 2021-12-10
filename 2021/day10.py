59a9e3d5b85511549400d44eca21e3a2f8f97f09
Code-Apprentice <codeguru42@gmail.com>
2021-12-09 22:36:08 -0700

    AOC 2021, Day 10: Solve Part 1

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,5 +1,35 @@
-def part1():
-    pass
+def is_corrupt(line):
+    opens = '{([<'
+    closes = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>',
+    }
+    s = []
+
+    for c in line:
+        if c in opens:
+            s.append(c)
+        else:
+            d = s.pop()
+            if c != closes[d]:
+                return c
+
+
+def corrupt_lines(file):
+    for line in file:
+        yield is_corrupt(line.strip())
+
+
+def part1(file):
+    score = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    }
+    return sum(score[c] for c in corrupt_lines(file) if c)
 
 
 def part2():
@@ -7,8 +37,9 @@ def part2():
 
 
 def main():
-    print(part1())
-    print(part2())
+    with open('day10.txt') as file:
+        print(part1(file))
+        print(part2())
 
 
 if __name__ == '__main__':
