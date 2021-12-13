e9dd8b2c161f2f99495072cd172b7e128b715a0c
Code-Apprentice <codeguru42@gmail.com>
2021-12-12 22:27:53 -0700

    AOC 2021, Day 13: Solve Part 1

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,5 +1,42 @@
-def part1():
-    pass
+def parse_points(file):
+    for line in file:
+        if line == '\n':
+            break
+        x, y = line.strip().split(',')
+        yield int(x), int(y)
+
+
+def parse_instructions(file):
+    for line in file:
+        axis, value = line.strip().split()[2].split('=')
+        yield axis, int(value)
+
+
+def parse(file):
+    return set(parse_points(file)), list(parse_instructions(file))
+
+
+def fold(points, instruction):
+    new_points = set()
+    axis, value = instruction
+    for x, y in points:
+        if axis == 'x':
+            if x < value:
+                yield x, y
+            else:
+                yield 2*value - x, y
+        elif axis == 'y':
+            if y < value:
+                yield x, y
+            else:
+                yield x, 2*value - y
+        else:
+            print('WHAT THE FUCK HAPPENED?')
+
+
+def part1(points, instruction):
+    new_points = set(fold(points, instruction))
+    return len(new_points)
 
 
 def part2():
@@ -7,7 +44,9 @@ def part2():
 
 
 def main():
-    print(part1())
+    with open('day13.txt') as file:
+        points, instructions = parse(file)
+    print(part1(points, instructions[0]))
     print(part2())
 
 
