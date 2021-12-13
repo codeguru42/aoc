c493bb909955557be1b7e6f5f4ae96e2fc56c2c1
Code-Apprentice <codeguru42@gmail.com>
2021-12-12 22:02:14 -0700

    AOC 2021, Day 12: Parse input file

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,3 +1,37 @@
+import unittest
+from collections import defaultdict
+
+
+class TestDay12(unittest.TestCase):
+    def test_parse(self):
+        example = """start-A
+            start-b
+            A-c
+            A-b
+            b-d
+            A-end
+            b-end """
+        expected = {
+            'start': {'A', 'b'},
+            'A': {'start', 'b', 'c', 'end'},
+            'c': {'A'},
+            'b': {'start', 'A', 'd', 'end'},
+            'd': {'b'},
+            'end': {'A', 'b'},
+        }
+        actual = parse(example.split('\n'))
+        self.assertEqual(expected, actual)
+
+
+def parse(file):
+    g = defaultdict(lambda: set())
+    for line in file:
+        x, y = line.strip().split('-')
+        g[x].add(y)
+        g[y].add(x)
+    return g
+
+
 def part1():
     pass
 
@@ -7,9 +41,13 @@ def part2():
 
 
 def main():
+    with open('day12.txt') as file:
+        g = parse(file)
+        print(g)
     print(part1())
     print(part2())
 
 
 if __name__ == '__main__':
+    unittest.main(exit=False)
     main()
