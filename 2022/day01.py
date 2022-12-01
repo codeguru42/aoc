e1e2ca096c39db670e29fc1a9ab65ad853eb0fee
Code-Apprentice <codeguru42@gmail.com>
2022-11-30 21:58:42 -0700

    AoC 2022, Day 1: Stub

---
+++ 2022/day01.py
@@ -0,0 +1,19 @@
+from aocd import get_data
+
+data = get_data(year=2022, day=1)
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
