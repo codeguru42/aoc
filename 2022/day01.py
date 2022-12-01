8b2554294424efbc9117afd46a069bf6498f4e60
Code-Apprentice <codeguru42@gmail.com>
2022-11-30 22:10:59 -0700

    AoC 2022, Day 1: Solution to Part 2

--- 2022/day01.py
+++ 2022/day01.py
@@ -11,14 +11,15 @@ def part1(elves):
     return max(sum(elf) for elf in elves)
 
 
-def part2():
-    pass
+def part2(elves):
+    return sum(sorted(sum(elf) for elf in elves)[-3:])
 
 
 def main():
     data = get_data(year=2022, day=1)
     parsed = list(parse(data))
     print(part1(parsed))
+    print(part2(parsed))
 
 
 if __name__ == '__main__':
