de7d28d775c44f4ff7ff0a871d74a5e4dcb6ca52
Code-Apprentice <codeguru42@gmail.com>
2022-12-01 22:08:54 -0700

    AoC 2022, Day 2: Solve Part 1

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,8 +1,36 @@
 from aocd import get_data, submit
 
+score_my_move = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3,
+}
 
-def part1():
-    pass
+round_score = {
+    'X': {
+        'A': 3,
+        'B': 0,
+        'C': 6,
+    },
+    'Y': {
+        'A': 6,
+        'B': 3,
+        'C': 0,
+    },
+    'Z': {
+        'A': 0,
+        'B': 6,
+        'C': 3,
+    },
+}
+
+
+def parse(data):
+    return [line.split() for line in data.split('\n')]
+
+
+def part1(moves):
+    return sum(score_my_move[me] + round_score[me][you] for you, me in moves)
 
 
 def part2():
@@ -11,7 +39,8 @@ def part2():
 
 def main():
     data = get_data(year=2022, day=2)
-    answer1 = part1()
+    moves = parse(data)
+    answer1 = part1(moves)
     print(answer1)
     answer2 = part2()
     print(answer2)
