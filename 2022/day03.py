e136aeba77ca072495bbe0cc909f2578675eb146
Code-Apprentice <codeguru42@gmail.com>
2022-12-02 22:10:20 -0700

    AoC 2022, Day 3: Solve Part 1

--- 2022/day03.py
+++ 2022/day03.py
@@ -1,8 +1,22 @@
 from aocd import get_data
 
 
-def part1():
-    pass
+def parse(data):
+    return data.split()
+
+
+def part1(sacks):
+    total = 0
+    for sack in sacks:
+        n = len(sack)
+        c1 = set(sack[:int(n/2)])
+        c2 = set(sack[int(n/2):])
+        both = c1 & c2
+        for item in both:
+            total += ord(item.lower()) - ord('a') + 1
+            if item.isupper():
+                total += 26
+    return total
 
 
 def part2():
@@ -11,7 +25,8 @@ def part2():
 
 def main():
     data = get_data(year=2022, day=3)
-    answer1 = part1()
+    supplies = parse(data)
+    answer1 = part1(supplies)
     print(answer1)
     answer2 = part2()
     print(answer2)
