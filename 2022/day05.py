84fc07bf7edff93ed621d667c8096ce56c38f904
Code-Apprentice <codeguru42@gmail.com>
2022-12-04 23:03:23 -0700

    AoC 2022, Day 5: Solve Part 1

--- 2022/day05.py
+++ 2022/day05.py
@@ -1,4 +1,5 @@
 import re
+import unittest
 from itertools import zip_longest
 
 from aocd import get_data
@@ -26,15 +27,39 @@ def parse_inst(lines):
 
 def parse(data):
     lines = data.split('\n')
-    labels_index = lines.index(' 1   2   3   4   5   6   7   8   9 ')
+    stack_count = (len(lines[0]) + 1) // 4
+    labels = ' '.join(f' {i} ' for i in range(1, stack_count + 1))
+    labels_index = lines.index(labels)
     crate_lines = lines[:labels_index]
     inst_lines = lines[labels_index+2:]
 
     return parse_crates(crate_lines), list(parse_inst(inst_lines))
 
 
-def part1():
-    pass
+example = '''    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2'''
+
+
+class TestPart1(unittest.TestCase):
+    def test_part1(self):
+        stacks, inst = parse(example)
+        result = part1(stacks, inst)
+        self.assertEqual('CMZ', result)
+
+
+def part1(stacks, inst):
+    for count, stack_from, stack_to in inst:
+        stacks[stack_to - 1] += reversed(stacks[stack_from - 1][-count:])
+        stacks[stack_from - 1] = stacks[stack_from - 1][:-count]
+
+    return ''.join(stack[-1] for stack in stacks)
 
 
 def part2():
@@ -44,9 +69,7 @@ def part2():
 def main():
     data = get_data(year=2022, day=5)
     stacks, inst = parse(data)
-    print(stacks)
-    print(inst)
-    answer1 = part1()
+    answer1 = part1(stacks, inst)
     print(answer1)
     answer2 = part2()
     print(answer2)
