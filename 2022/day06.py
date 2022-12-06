adea069555b4ad5c49c02901502aff8164ff8270
Code-Apprentice <codeguru42@gmail.com>
2022-12-05 22:08:10 -0700

    AoC 2022, Day 6: Solve Part 1

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,8 +1,43 @@
+import collections
+import unittest
+from itertools import islice
+
 from aocd import get_data
 
 
-def part1():
-    pass
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+class TestPart1(unittest.TestCase):
+    def test1(self):
+        signal = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
+        self.assertEqual(5, part1(signal))
+
+    def test2(self):
+        signal = 'nppdvjthqldpwncqszvftbrmjlhg'
+        self.assertEqual(6, part1(signal))
+
+    def test3(self):
+        signal = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
+        self.assertEqual(10, part1(signal))
+
+    def test4(self):
+        signal = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
+        self.assertEqual(11, part1(signal))
+
+
+def part1(signal):
+    for i, packet in enumerate(sliding_window(signal, 4)):
+        if len(set(packet)) == len(packet):
+            return i + len(packet)
 
 
 def part2():
@@ -11,7 +46,7 @@ def part2():
 
 def main():
     data = get_data(year=2022, day=6)
-    answer1 = part1()
+    answer1 = part1(data)
     print(answer1)
     answer2 = part2()
     print(answer2)
