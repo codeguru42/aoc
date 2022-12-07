bd2353c9e25e2be0617fcda2d12537a3b29a9da6
Code-Apprentice <codeguru42@gmail.com>
2022-12-06 23:32:12 -0600

    AOC 2022 Day 7: Build directory tree from input

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,6 +1,42 @@
 from aocd import get_data
 
 
+def parse(data):
+    lines = iter(data.split('\n'))
+    fs = {}
+    build_fs(lines, fs, fs)
+    return fs
+
+
+def populate_dir(current_dir, commands):
+    file_spec = next(commands, None)
+    while file_spec is not None and not file_spec.startswith('$'):
+        size, name = file_spec.split()
+        if size == 'dir':
+            current_dir[name] = {'..': current_dir}
+        else:
+            current_dir[name] = size
+        file_spec = next(commands, None)
+    return file_spec
+
+
+def build_fs(commands, root_dir, current_dir):
+    command = next(commands, None)
+    while command is not None:
+        parts = command.split()
+        if parts[1] == 'ls':
+            command = populate_dir(current_dir, commands)
+        elif parts[1] == 'cd':
+            if parts[2] == '/':
+                build_fs(commands, root_dir, root_dir)
+            else:
+                build_fs(commands, root_dir, current_dir[parts[2]])
+            command = next(commands, None)
+        else:
+            print("Invalid command:", parts[1])
+            command = next(commands, None)
+
+
 def part1():
     pass
 
@@ -8,9 +44,10 @@ def part1():
 def part2():
     pass
 
-
 def main():
     data = get_data(year=2022, day=7)
+    fs = parse(data)
+    print(fs)
     answer1 = part1()
     print(answer1)
     answer2 = part2()
