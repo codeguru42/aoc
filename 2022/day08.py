c818e60b4e3b4e0b181fefb93369069f60ee2366
Code-Apprentice <codeguru42@gmail.com>
2022-12-08 00:21:36 -0600

    AOC 2022 Day 8: Stub

---
+++ 2022/day08.py
@@ -0,0 +1,21 @@
+from aocd import get_data
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+def main():
+    data = get_data(year=2022, day=8)
+    answer1 = part1()
+    print(answer1)
+    answer2 = part2()
+    print(answer2)
+
+
+if __name__ == '__main__':
+    main()
