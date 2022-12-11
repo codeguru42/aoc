a09db9bdd3fc0e9120de0ab0e08c17d82472bbf6
Code-Apprentice <codeguru42@gmail.com>
2022-12-11 02:09:04 -0600

    AOC 2022 Day 8: Finish solving part 1

--- 2022/day08.py
+++ 2022/day08.py
@@ -16,7 +16,7 @@ def parse(data):
 
 class TestPart1(unittest.TestCase):
     def test1(self):
-        self.assertEqual(21, part1(example.split('\n')))
+        self.assertEqual(21, part1(example.strip().split('\n')))
 
     def test_increasing_subsequence1(self):
         row = example.split('\n')[1]
@@ -33,6 +33,56 @@ class TestPart1(unittest.TestCase):
         expected = [(0, '3'), (2, '5'), (4, '9')]
         self.assertEqual(expected, list(increasing_subsequence(row)))
 
+    def test_get_visible_rows(self):
+        forest = example.split('\n')
+        expected = [
+            ((0, 0), '3'),
+            ((0, 3), '7'),
+            ((1, 0), '2'),
+            ((1, 1), '5'),
+            ((2, 0), '6'),
+            ((3, 0), '3'),
+            ((3, 2), '5'),
+            ((3, 4), '9'),
+            ((4, 0), '3'),
+            ((4, 1), '5'),
+            ((4, 3), '9'),
+        ]
+        self.assertEqual(expected, list(get_visible(forest)))
+
+    def test_get_visible_rows_reversed(self):
+        forest = [reversed(row) for row in example.split('\n')]
+        expected = [
+            ((0, 4), '3'),
+            ((0, 3), '7'),
+            ((1, 4), '2'),
+            ((1, 2), '5'),
+            ((2, 4), '2'),
+            ((2, 3), '3'),
+            ((2, 1), '5'),
+            ((2, 0), '6'),
+            ((3, 4), '9'),
+            ((4, 4), '0'),
+            ((4, 3), '9'),
+        ]
+        self.assertEqual(expected, list(get_visible(forest, reversed=True)))
+
+    def test_get_visible_rows_transposed(self):
+        forest = zip(*example.strip().split('\n'))
+        expected = [
+            ((0, 0), '3'),
+            ((2, 0), '6'),
+            ((0, 1), '0'),
+            ((1, 1), '5'),
+            ((0, 2), '3'),
+            ((1, 2), '5'),
+            ((0, 3), '7'),
+            ((4, 3), '9'),
+            ((0, 4), '3'),
+            ((3, 4), '9'),
+        ]
+        self.assertEqual(expected, list(get_visible(forest, transposed=True)))
+
 
 def increasing_subsequence(row):
     highest = 0
@@ -42,10 +92,14 @@ def increasing_subsequence(row):
             yield i, height
 
 
-def get_visible(forest, transposed=False):
-    for j, row in enumerate(forest):
+def get_visible(forest, reversed=False, transposed=False):
+    for i, row in enumerate(forest):
+        row = list(row)
+        n = len(row)
         sub = increasing_subsequence(row)
-        for i, height in sub:
+        for j, height in sub:
+            if reversed:
+                j = n - j - 1
             if transposed:
                 yield (j, i), height
             else:
@@ -54,10 +108,10 @@ def get_visible(forest, transposed=False):
 
 def part1(forest):
     visible = set(get_visible(forest))
-    visible |= set(get_visible(reversed(row) for row in forest))
+    visible |= set(get_visible((reversed(row) for row in forest), reversed=True))
     transposed = list(zip(*forest))
     visible |= set(get_visible(transposed, transposed=True))
-    visible |= set(get_visible((reversed(row) for row in transposed), transposed=True))
+    visible |= set(get_visible((reversed(row) for row in transposed), reversed=True, transposed=True))
     return len(visible)
 
 
