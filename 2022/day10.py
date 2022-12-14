fbf00d63d09e63994b155b03b1d081d15f2ee219
Code-Apprentice <codeguru42@gmail.com>
2022-12-13 20:15:52 -0700

    AoC 2022, Day 10: Parse input

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,6 +1,11 @@
 from aocd import get_data
 
 
+def parse(data: str):
+    for line in data.strip().split('\n'):
+        yield line.split()
+
+
 def part1():
     pass
 
@@ -11,6 +16,7 @@ def part2():
 
 def main():
     data = get_data(year=2022, day=10)
+    inst = list(parse(data))
     answer1 = part1()
     print(answer1)
     answer2 = part2()
