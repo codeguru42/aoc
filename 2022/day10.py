74fd60b9077282994e3b013f912ff24516ff8b53
Code-Apprentice <codeguru42@gmail.com>
2022-12-13 20:35:38 -0700

    AoC 2022, Day 10: Solve Part 1

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,3 +1,5 @@
+import unittest
+
 from aocd import get_data
 
 
@@ -6,8 +8,38 @@ def parse(data: str):
         yield line.split()
 
 
-def part1():
-    pass
+example = '''noop
+addx 3
+addx -5
+'''
+
+
+class TestPart1(unittest.TestCase):
+    def test_evaluate(self):
+        inst = parse(example)
+        result = evaluate(inst)
+        expected = [1, 2, 3, 16, 20]
+        self.assertEqual(expected, list(result))
+
+
+def evaluate(instructions):
+    clock = 1
+    x = 1
+    for inst in instructions:
+        match inst:
+            case 'noop', :
+                yield clock * x
+                clock += 1
+            case 'addx', amt:
+                yield clock * x
+                clock += 1
+                yield clock * x
+                x += int(amt)
+                clock += 1
+
+
+def part1(instructions):
+    return sum(list(evaluate(instructions))[19:220:40])
 
 
 def part2():
@@ -17,7 +49,7 @@ def part2():
 def main():
     data = get_data(year=2022, day=10)
     inst = list(parse(data))
-    answer1 = part1()
+    answer1 = part1(inst)
     print(answer1)
     answer2 = part2()
     print(answer2)
