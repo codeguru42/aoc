af7dc0770b9e26e97987ba1ad09dc7390ca36f37
Code-Apprentice <codeguru42@gmail.com>
2022-12-23 14:25:53 -0700

    AoC 2022, Day 13: Start to solve Part 1

--- 2022/day13.py
+++ 2022/day13.py
@@ -8,8 +8,28 @@ def parse(data):
         yield [ast.literal_eval(line) for line in pair.split('\n')]
 
 
-def part1():
-    pass
+def is_in_order(p1, p2):
+    match p1, p2:
+        case [x1, *rest1], [x2, *rest2]:
+            match x1, x2:
+                case [int(), int()]:
+                    return x1 < x2 and is_in_order(rest1, rest2)
+                case [list(), list()]:
+                    return is_in_order(x1, x2) and is_in_order(rest1, rest2)
+                case [int(), list()]:
+                    return is_in_order([x1], x2) and is_in_order(rest1, rest2)
+                case [list(), int()]:
+                    return is_in_order(x1, [x2]) and is_in_order(rest1, rest2)
+
+
+def get_indexes(packets):
+    for i, p in enumerate(packets):
+        if is_in_order(*p):
+            yield i
+
+
+def part1(packets):
+    return sum(get_indexes(packets))
 
 
 def part2():
@@ -19,8 +39,7 @@ def part2():
 def main():
     data = get_data(year=2022, day=13)
     packets = parse(data)
-    print(list(packets))
-    answer1 = part1()
+    answer1 = part1(packets)
     print(answer1)
     answer2 = part2()
     print(answer2)
