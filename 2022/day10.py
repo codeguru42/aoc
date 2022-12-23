ca2adcda67ecb74f98880e2ac83d4c65786f488b
Code-Apprentice <codeguru42@gmail.com>
2022-12-22 18:40:46 -0700

    AoC 2022, Day 10: Attempt to solve part 2

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,5 +1,5 @@
 import unittest
-from itertools import islice
+from itertools import islice, zip_longest
 
 from aocd import get_data
 
@@ -7,6 +7,11 @@ from aocd import get_data
 def parse(data: str):
     for line in data.strip().split('\n'):
         yield line.split()
+
+
+def grouper(iterable, n, fillvalue=None):
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 
 example = '''noop
@@ -43,8 +48,181 @@ def part1(instructions):
     return sum(islice((clock * x for clock, x in evaluate(instructions)), 19, 220, 40))
 
 
-def part2():
-    pass
+def scan_line(seq):
+    for clock, x in seq:
+        if (clock - 1) % 40 <= x <= (clock + 1) % 40:
+            yield '#'
+        else:
+            yield '.'
+
+
+example2 = '''addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+
+expected_scanline = '''##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+'''
+
+
+class TestPart2(unittest.TestCase):
+    def test_part2(self):
+        inst = parse(example2)
+        result = part2(inst)
+        self.assertEqual(expected_scanline, result)
+
+
+def part2(instructions):
+    lines = scan_line(evaluate(instructions))
+    return '\n'.join(''.join(line) for line in grouper(lines, 40))
 
 
 def main():
@@ -52,7 +230,7 @@ def main():
     inst = list(parse(data))
     answer1 = part1(inst)
     print(answer1)
-    answer2 = part2()
+    answer2 = part2(inst)
     print(answer2)
 
 
