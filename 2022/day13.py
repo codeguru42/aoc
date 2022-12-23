58b0cb9864593722de4826fd4272ede36c1e20b7
Code-Apprentice <codeguru42@gmail.com>
2022-12-23 15:43:30 -0700

    AoC 2022, Day 13: Simplify match statements

--- 2022/day13.py
+++ 2022/day13.py
@@ -45,16 +45,14 @@ def parse(data):
 
 def is_in_order(p1, p2):
     match p1, p2:
-        case [x1, *rest1], [x2, *rest2]:
-            match x1, x2:
-                case [int(), int()]:
-                    return x1 < x2 or x1 == x2 and is_in_order(rest1, rest2)
-                case [list(), list()]:
-                    return is_in_order(x1, x2) and is_in_order(rest1, rest2)
-                case [int(), list()]:
-                    return is_in_order([x1], x2) and is_in_order(rest1, rest2)
-                case [list(), int()]:
-                    return is_in_order(x1, [x2]) and is_in_order(rest1, rest2)
+        case [int(x1), *rest1], [int(x2), *rest2]:
+            return x1 < x2 or x1 == x2 and is_in_order(rest1, rest2)
+        case[list(x1), *rest1], [list(x2), *rest2]:
+            return is_in_order(x1, x2) and is_in_order(rest1, rest2)
+        case[int(x1), *rest1], [list(x2), *rest2]:
+            return is_in_order([x1], x2) and is_in_order(rest1, rest2)
+        case[list(x1), *rest1], [int(x2), *rest2]:
+            return is_in_order(x1, [x2]) and is_in_order(rest1, rest2)
 
 
 def get_indexes(packets):
