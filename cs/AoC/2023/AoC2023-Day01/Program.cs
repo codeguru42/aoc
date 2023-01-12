27b944ece643be0ed7b7dd4fd3948d830025fa8c
Code-Apprentice <codeguru42@gmail.com>
2023-01-12 11:18:54 -0700

    AoC 2023, Day 1: Read input file

--- cs/AoC/2023/AoC2023-Day01/Program.cs
+++ cs/AoC/2023/AoC2023-Day01/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var input = System.IO.File.ReadAllText("input.txt");
+Console.WriteLine(input);
