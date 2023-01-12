f6e136dbcb12d22bee2a36eb2d6b5d91fe4e3330
Code-Apprentice <codeguru42@gmail.com>
2023-01-12 12:16:45 -0700

    AoC 2023, Day 1: Solve Part 1 with C#

--- cs/AoC/2023/AoC2023-Day01/Program.cs
+++ cs/AoC/2023/AoC2023-Day01/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var input = System.IO.File.ReadAllText("input.txt");
-Console.WriteLine(input);
+using static System.Int32;
+
+var input = File.ReadAllText("input.txt");
+var elves = input.Split("\n\n");
+var calories = elves.Select(elf =>
+{
+    var calories = elf.Trim().Split('\n').Select(Parse);
+    return calories.Sum();
+});
+Console.WriteLine(calories.Max());
