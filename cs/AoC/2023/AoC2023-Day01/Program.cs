dc3c0aa14d6600b5f63b37131661d771df3a8faf
Code-Apprentice <codeguru42@gmail.com>
2023-01-12 14:04:03 -0700

    AoC 2023, Day 1: Solve Part 2

--- cs/AoC/2023/AoC2023-Day01/Program.cs
+++ cs/AoC/2023/AoC2023-Day01/Program.cs
@@ -2,8 +2,10 @@
 
 var input = File.ReadAllText("input.txt");
 var elves = Parse(input);
-var answer = Part1(elves);
-Console.WriteLine(answer);
+var answer1 = Part1(elves);
+Console.WriteLine(answer1);
+var answer2 = Part2(elves);
+Console.WriteLine(answer2);
 
 IEnumerable<IEnumerable<int>> Parse(string input)
 {
@@ -15,4 +17,10 @@ int Part1(IEnumerable<IEnumerable<int>> elves)
 {
     var calories = elves.Select(elf => elf.Sum());
     return calories.Max();
+}
+
+int Part2(IEnumerable<IEnumerable<int>> elves)
+{
+    var calories = elves.Select(elf => elf.Sum()).OrderByDescending(c => c).ToArray();
+    return calories[..3].Sum();
 }
