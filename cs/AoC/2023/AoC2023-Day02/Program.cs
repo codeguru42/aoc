560e366b1db9a923fbcf9759ae68f3c97985ff99
Code-Apprentice <codeguru42@gmail.com>
2023-03-01 22:57:03 -0700

    AoC 2023, Day 2: Solve Part 2

--- cs/AoC/2023/AoC2023-Day02/Program.cs
+++ cs/AoC/2023/AoC2023-Day02/Program.cs
@@ -8,6 +8,13 @@
     {"Z", 'S'},
 };
 
+var StrategyMap = new Dictionary<string, Dictionary<char, char>>()
+{
+    {"X", new Dictionary<char, char>() {{'R', 'S'}, {'P', 'R'}, {'S', 'P'}}},
+    {"Y", new Dictionary<char, char>() {{'R', 'R'}, {'P', 'P'}, {'S', 'S'}}},
+    {"Z", new Dictionary<char, char>() {{'R', 'P'}, {'P', 'S'}, {'S', 'R'}}},
+};
+
 var WinPoints = new Dictionary<char, Dictionary<char, int>>()
 {
     {'R', new Dictionary<char, int>() {{'R', 3}, {'P', 6}, {'S', 0}}},
@@ -26,25 +33,36 @@ var input = File.ReadAllText("input.txt");
 var data = Parse(input);
 var answer1 = Part1(data);
 Console.WriteLine(answer1);
+var answer2 = Part2(data);
+Console.WriteLine(answer2);
 
-IEnumerable<IEnumerable<char>> Parse(string input) =>
+IEnumerable<string[]> Parse(string input) =>
     input.Trim().Split('\n')
         .Select(line =>
-            line.Trim().Split(' ').Select(c => MovesMap[c])
+            line.Trim().Split(' ')
         );
 
-int Part1(IEnumerable<IEnumerable<char>> data)
+int Part1(IEnumerable<string[]> data)
 {
     var scores = data
         .Select(round =>
             {
-                var moves = round.ToArray();
+                var moves = round.Select(r => MovesMap[r]).ToArray();
                 return MovePoints[moves[1]] + WinPoints[moves[0]][moves[1]];
             }
         );
     return scores.Sum();
 }
 
-void Part2()
+int Part2(IEnumerable<string[]> data)
 {
+    var scores = data
+        .Select(round =>
+            {
+                var you = MovesMap[round[0]];
+                var me = StrategyMap[round[1]][you];
+                return MovePoints[me] + WinPoints[you][me];
+            }
+        );
+    return scores.Sum();
 }
