8f6bd14bdcc30b7f2c3a4b1d4187727c0653247f
Code-Apprentice <codeguru42@gmail.com>
2023-04-04 00:26:14 -0600

    AoC 2023, Day 3: Parse input

--- cs/AoC/2023/AoC2023-Day03/Program.cs
+++ cs/AoC/2023/AoC2023-Day03/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var input = File.ReadAllText("input.txt");
+var data = Parse(input);
 
-Console.WriteLine(input);
+foreach (var rucksack in data)
+{
+    Console.WriteLine($"{rucksack[0]} {rucksack[1]}");
+}
+
+IEnumerable<string[]> Parse(string input)
+{
+    return input.Trim().Split('\n')
+        .Select(line =>
+        {
+            var half = line.Length / 2;
+            return new string[]
+            {
+                line[..half],
+                line[half..]
+            };
+        });
+}
