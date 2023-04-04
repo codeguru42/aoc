0edbda9f5d902783c660a5f45b16478fdb9f0134
Code-Apprentice <codeguru42@gmail.com>
2023-04-04 16:57:44 -0600

    AoC 2023, Day 3: Solve Part 2

--- cs/AoC/2023/AoC2023-Day03/Program.cs
+++ cs/AoC/2023/AoC2023-Day03/Program.cs
@@ -1,35 +1,49 @@
 ﻿using static System.Char;
 
 var input = File.ReadAllText("input.txt");
+
 var data = Parse(input);
 
 var answer1 = Part1(data);
+
 Console.WriteLine(answer1);
 
-IEnumerable<string[]> Parse(string input)
+var answer2 = Part2(data);
+
+Console.WriteLine(answer2);
+
+string[] Parse(string input)
 {
-    return input.Trim().Split('\n')
-        .Select(line =>
-        {
-            var half = line.Length / 2;
-            return new string[]
-            {
-                line[..half],
-                line[half..]
-            };
-        });
+    return input.Trim().Split('\n');
 }
 
-int Part1(IEnumerable<string[]> rucksacks)
+int Score(char item)
+{
+    if (IsLower(item))
+    {
+        return item - 'a' + 1;
+    }
+
+    return item - 'A' + 27;
+}
+
+int Part1(IEnumerable<string> rucksacks)
 {
     var priorities = rucksacks.Select(rucksack =>
     {
-        var common = rucksack[0].Intersect(rucksack[1]).First();
-        if (IsLower(common))
-        {
-            return common - 'a' + 1;
-        }
-        return common - 'A' + 27;
+        var half = rucksack.Length / 2;
+        var common = rucksack[..half].Intersect(rucksack[half..]).First();
+        return Score(common);
+    });
+    return priorities.Sum();
+}
+
+int Part2(IEnumerable<string> rucksacks)
+{
+    var priorities = rucksacks.Chunk(3).Select(rucksack =>
+    {
+        var common = rucksack[0].Intersect(rucksack[1]).Intersect(rucksack[2]).First();
+        return Score(common);
     });
     return priorities.Sum();
 }
