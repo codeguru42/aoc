d990967f46d27877aa12e40bcbb1ba8f8c823557
Code-Apprentice <codeguru42@gmail.com>
2023-04-04 18:32:00 -0600

    AoC 2023, Day 4: Stubs

--- cs/AoC/2022/AoC2022-Day04/Program.cs
+++ cs/AoC/2022/AoC2022-Day04/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var input = File.ReadAllText("input.txt");
+var data = Parse(input);
 
-Console.WriteLine("Hello, World!");
+var answer1 = Part1(data);
+Console.WriteLine(answer1);
+
+var answer2 = Part2(data);
+Console.WriteLine(answer2);
+
+object Parse(string s)
+{
+    throw new NotImplementedException();
+}
+
+bool Part1(object o)
+{
+    throw new NotImplementedException();
+}
+
+bool Part2(object o)
+{
+    throw new NotImplementedException();
+}
