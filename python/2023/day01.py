20e1410509131db0baab8a736125f94db76ed12a
Code-Apprentice <codeguru42@gmail.com>
2023-11-30 22:07:49 -0700

    AoC 2023, Day 1: Solve Part 1

---
+++ python/2023/day01.py
@@ -0,0 +1,26 @@
+from aocd import get_data
+
+
+def parse(data):
+    return data.split()
+
+
+def part1(lines):
+    for line in lines:
+        digits = [c for c in line if c.isdigit()]
+        yield int(digits[0] + digits[-1])
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    data = get_data(year=2023, day=1)
+    parsed = parse(data)
+    print(sum(part1(parsed)))
+    print(part2(parsed))
+
+
+if __name__ == '__main__':
+    main()
