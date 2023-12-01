4afb00ef9910ab5937b67849a509e7e8e414dc9c
Code-Apprentice <codeguru42@gmail.com>
2023-11-30 22:10:54 -0700

    AoC 2023, Day 1: Refactor Part 1

--- python/2023/day01.py
+++ python/2023/day01.py
@@ -6,6 +6,10 @@ def parse(data):
 
 
 def part1(lines):
+    return sum(digits1(lines))
+
+
+def digits1(lines):
     for line in lines:
         digits = [c for c in line if c.isdigit()]
         yield int(digits[0] + digits[-1])
@@ -18,7 +22,7 @@ def part2(lines):
 def main():
     data = get_data(year=2023, day=1)
     parsed = parse(data)
-    print(sum(part1(parsed)))
+    print(part1(parsed))
     print(part2(parsed))
 
 
