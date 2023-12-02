85784d4073d4bd957d8155bc763ea9cc5d8afb24
Code-Apprentice <codeguru42@gmail.com>
2023-12-01 22:34:36 -0700

    AoC 2023, Day 2: Refactor with dataclasses

--- python/2023/day02.py
+++ python/2023/day02.py
@@ -1,42 +1,55 @@
-from collections import defaultdict
+from dataclasses import dataclass
 
 from aocd import get_data
 
 
-def parse_sets(sets):
+@dataclass
+class GameSet:
+    red: int = 0
+    blue: int = 0
+    green: int = 0
+
+
+@dataclass
+class Game:
+    id: int
+    game_sets: list[GameSet]
+
+
+def parse_sets(sets: list[str]) -> list[GameSet]:
     for s in sets:
-        d = {"red": 0, "green": 0, "blue": 0}
+        d = {}
         cubes = s.split(",")
         for cube in cubes:
             count, color = cube.strip().split(" ")
             d[color] = int(count)
-        yield d
+        yield GameSet(**d)
 
 
-def parse_line(line):
+def parse_line(line: str) -> Game:
     game, cubes = line.split(":")
     game_id = game.split(" ")[1]
     sets = cubes.strip().split(";")
-    return int(game_id), list(parse_sets(sets))
+    return Game(id=int(game_id), game_sets=list(parse_sets(sets)))
 
 
-def parse(data):
+def parse(data) -> list[Game]:
     lines = data.split("\n")
     for line in lines:
         yield parse_line(line)
 
 
-def get_possible(games):
-    for game_id, sets in games:
+def get_possible(games: list[Game]):
+    for game in games:
         possible = True
-        for s in sets:
-            if s["red"] > 12 or s["green"] > 13 or s["blue"] > 14:
+        for s in game.game_sets:
+            if s.red > 12 or s.green > 13 or s.blue > 14:
                 possible = False
         if possible:
-            yield game_id
+            yield game.id
 
 
-def part1(games):
+def part1(games: list[Game]):
     return sum(get_possible(games))
 
 
