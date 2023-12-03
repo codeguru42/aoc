a5370d6d034861ae3fc9534f035f438bf0cf7ba9
Code-Apprentice <codeguru42@gmail.com>
2023-12-02 22:32:23 -0700

    AoC 2023, Day 3: Parse part numbers

--- python/2023/day03.py
+++ python/2023/day03.py
@@ -1,8 +1,59 @@
+from dataclasses import dataclass
+
 from aocd import get_data
+
+example_schematic = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+"""
+
+
+def test_parse_part_numbers():
+    pns = parse_part_numbers(example_schematic.split("\n"))
+    expected = [
+        PartNumber(part_number=467, position=(0, 0)),
+        PartNumber(part_number=114, position=(0, 5)),
+        PartNumber(part_number=35, position=(2, 2)),
+        PartNumber(part_number=633, position=(2, 6)),
+        PartNumber(part_number=617, position=(4, 0)),
+        PartNumber(part_number=58, position=(5, 7)),
+        PartNumber(part_number=592, position=(6, 2)),
+        PartNumber(part_number=755, position=(7, 6)),
+        PartNumber(part_number=664, position=(9, 1)),
+        PartNumber(part_number=598, position=(9, 5)),
+    ]
+    assert list(pns) == expected
+
+
+@dataclass
+class PartNumber:
+    part_number: int
+    position: tuple[int, int]
 
 
 def parse(data) -> list[str]:
     return data.split("\n")
+
+
+def parse_part_numbers(schematic) -> list[PartNumber]:
+    for i, line in enumerate(schematic):
+        part_number = 0
+        position = (-1, -1)
+        for j, c in enumerate(line):
+            if c.isdigit():
+                if part_number == 0:
+                    position = (i, j)
+                part_number = 10 * part_number + int(c)
+            elif part_number != 0:
+                yield PartNumber(part_number=part_number, position=position)
+                part_number = 0
 
 
 def part1(games: list[str]):
@@ -15,7 +66,7 @@ def part2(games: list[str]):
 
 def main():
     data = get_data(year=2023, day=3)
-    parsed = parse(data)
+    parsed = list(parse(data))
     print(part1(parsed))
     print(part2(parsed))
 
