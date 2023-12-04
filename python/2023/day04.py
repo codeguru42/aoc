8531e81631b4eddad01ed0da84a317656f633057
Code-Apprentice <codeguru42@gmail.com>
2023-12-03 22:50:51 -0700

    AoC 2023, Day 4: Parse input

--- python/2023/day04.py
+++ python/2023/day04.py
@@ -1,8 +1,30 @@
+from dataclasses import dataclass
+
 from aocd import get_data
 
 
-def parse(data):
-    return data.split()
+@dataclass
+class Card:
+    card_id: int
+    winning: list[int]
+    numbers: list[int]
+
+
+def parse_numbers(numbers: str):
+    return [int(n) for n in numbers.strip().split()]
+
+
+def parse(data: str) -> list[Card]:
+    lines = data.strip().split("\n")
+    for line in lines:
+        card, card_data = line.split(":")
+        card_id = card.split(" ")[-1]
+        winning, numbers = card_data.split("|")
+        yield Card(
+            card_id=card_id,
+            winning=parse_numbers(winning),
+            numbers=parse_numbers(numbers),
+        )
 
 
 def part1(lines):
@@ -15,8 +37,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=4)
-    print(data)
-    parsed = parse(data)
+    parsed = list(parse(data))
+    print(parsed)
     print(part1(parsed))
     print(part2(parsed))
 
