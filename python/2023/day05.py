a7157769118cf668460f0f6f1bc1c65e2736bd86
Code-Apprentice <codeguru42@gmail.com>
2023-12-04 22:26:56 -0700

    AoC 2023, Day 5: Parse input

--- python/2023/day05.py
+++ python/2023/day05.py
@@ -1,24 +1,55 @@
+from dataclasses import dataclass
+
 from aocd import get_data
 
 
+@dataclass
+class Map:
+    dest: int
+    source: int
+    len: int
+
+
+def parse_map(map):
+    lines = map.split("\n")
+    for line in lines[1:]:
+        source, dest, l = [int(x) for x in line.split()]
+        yield Map(source=source, dest=dest, len=l)
+
+
+def parse_maps(maps):
+    for map in maps[1:]:
+        yield parse_map(map)
+
+
+def parse_seeds(seeds: str):
+    _, seeds = seeds.split(":")
+    return [int(s) for s in seeds.strip().split(" ")]
+
+
 def parse(data):
-    return data.split()
+    maps = data.split("\n\n")
+    return parse_seeds(maps[0]), list(parse_maps(maps[1:]))
 
 
-def part1(lines):
+def part1(seeds, maps):
     pass
 
 
-def part2(lines):
+def part2(seeds, maps):
     pass
 
 
 def main():
     data = get_data(year=2023, day=5)
-    print(data)
-    parsed = parse(data)
-    print(part1(parsed))
-    print(part2(parsed))
+    seeds, maps = parse(data)
+    print(seeds)
+    for map in maps:
+        print("----------")
+        for m in map:
+            print(m)
+    print(part1(seeds, maps))
+    print(part2(seeds, maps))
 
 
 if __name__ == "__main__":
