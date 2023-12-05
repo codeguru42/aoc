7f513180453237e54e5794faed04a49a95cf11c5
Code-Apprentice <codeguru42@gmail.com>
2023-12-04 22:53:42 -0700

    AoC 2023, Day 5: Solve Part 1

--- python/2023/day05.py
+++ python/2023/day05.py
@@ -1,6 +1,64 @@
 from dataclasses import dataclass
 
+import pytest
 from aocd import get_data
+
+example = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"""
+
+
+@pytest.fixture
+def data():
+    return parse(example.strip())
+
+
+def test_part1(data):
+    seeds, maps = data
+    assert part1(seeds, maps) == 35
+
+
+@pytest.fixture(params=range(4))
+def seed(request, data):
+    expected_locations = [82, 43, 86, 35]
+    seeds, _ = data
+    return seeds[request.param], expected_locations[request.param]
+
+
+def test_get_location(seed, data):
+    _, maps = data
+    assert get_location(seed[0], maps) == seed[1]
 
 
 @dataclass
@@ -13,13 +71,13 @@ class Map:
 def parse_map(map):
     lines = map.split("\n")
     for line in lines[1:]:
-        source, dest, l = [int(x) for x in line.split()]
+        dest, source, l = [int(x) for x in line.split()]
         yield Map(source=source, dest=dest, len=l)
 
 
 def parse_maps(maps):
-    for map in maps[1:]:
-        yield parse_map(map)
+    for map in maps:
+        yield list(parse_map(map))
 
 
 def parse_seeds(seeds: str):
@@ -32,8 +90,22 @@ def parse(data):
     return parse_seeds(maps[0]), list(parse_maps(maps[1:]))
 
 
-def part1(seeds, maps):
-    pass
+def get_next(curr: int, map: list[Map]) -> int:
+    for m in map:
+        if m.source <= curr < m.source + m.len:
+            return m.dest + curr - m.source
+    return curr
+
+
+def get_location(seed: int, maps: list[list[Map]]) -> int:
+    curr = seed
+    for map in maps:
+        curr = get_next(curr, map)
+    return curr
+
+
+def part1(seeds: list[int], maps: list[list[Map]]):
+    return min(get_location(seed, maps) for seed in seeds)
 
 
 def part2(seeds, maps):
@@ -43,11 +115,6 @@ def part2(seeds, maps):
 def main():
     data = get_data(year=2023, day=5)
     seeds, maps = parse(data)
-    print(seeds)
-    for map in maps:
-        print("----------")
-        for m in map:
-            print(m)
     print(part1(seeds, maps))
     print(part2(seeds, maps))
 
