99ba64aa68e79e6c8de4d2af547af238f73db23d
Code-Apprentice <codeguru42@gmail.com>
2023-12-05 22:06:24 -0700

    AoC 2023, Day 6: Parse input

--- python/2023/day06.py
+++ python/2023/day06.py
@@ -1,8 +1,20 @@
+import re
+from dataclasses import dataclass
+
 from aocd import get_data
 
 
+@dataclass
+class Race:
+    time: int
+    distance: int
+
+
 def parse(data):
-    return data.split()
+    lines = data.split("\n")
+    times, distances = [re.split(r"\s+", line) for line in lines]
+    for t, d in zip(times[1:], distances[1:]):
+        yield Race(time=int(t), distance=int(d))
 
 
 def part1(lines):
@@ -15,8 +27,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=6)
-    print(data)
-    parsed = parse(data)
+    parsed = list(parse(data))
+    print(parsed)
     print(part1(parsed))
     print(part2(parsed))
 
