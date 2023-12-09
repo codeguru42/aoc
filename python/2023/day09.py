3f4ad46a7c2ec8e915d0555f3494334dd5d6ab7b
Code-Apprentice <codeguru42@gmail.com>
2023-12-08 22:14:16 -0700

    AoC 2023, Day 9: Solve Part 1

--- python/2023/day09.py
+++ python/2023/day09.py
@@ -1,4 +1,6 @@
+import collections
 import timeit
+from itertools import islice
 
 from aocd import get_data
 
@@ -9,8 +11,34 @@ def parse(data):
         yield [int(n) for n in line.split()]
 
 
-def part1(lines):
-    pass
+def sliding_window(iterable, n):
+    # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
+    it = iter(iterable)
+    window = collections.deque(islice(it, n - 1), maxlen=n)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+def diffs(history):
+    for x, y in sliding_window(history, 2):
+        yield y - x
+
+
+def all_diffs(history):
+    nums = history
+    while any(x != 0 for x in nums):
+        yield nums
+        nums = list(diffs(nums))
+
+
+def predict_next(history):
+    ds = all_diffs(history)
+    return sum(d[-1] for d in ds)
+
+
+def part1(histories):
+    return sum(predict_next(history) for history in histories)
 
 
 def part2(lines):
@@ -20,7 +48,6 @@ def part2(lines):
 def main():
     data = get_data(year=2023, day=9)
     parsed = list(parse(data))
-    print(parsed)
     print(part1(parsed))
     print(part2(parsed))
     print("Part 1:", timeit.timeit(lambda: part1(parsed), number=1))
