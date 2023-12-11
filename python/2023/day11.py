c91ff66e9327427aeec2b6224b37d3691fa2427f
Code-Apprentice <codeguru42@gmail.com>
2023-12-10 22:30:44 -0700

    AoC 2023, Day 11: Expand the universe!

--- python/2023/day11.py
+++ python/2023/day11.py
@@ -7,19 +7,39 @@ def parse(data):
     return data.split("\n")
 
 
-def part1(lines):
+def expand_rows(universe):
+    for row in universe:
+        yield row
+        if all(c == "." for c in row):
+            yield row
+
+
+def expand_columns(universe):
+    transposed = zip(*universe)
+    yield from expand_rows(transposed)
+
+
+def expand(universe):
+    universe = list(expand_rows(universe))
+    return expand_columns(universe)
+
+
+def part1(universe):
     pass
 
 
-def part2(lines):
+def part2(universe):
     pass
 
 
 def main():
     data = get_data(year=2023, day=11)
     parsed = parse(data)
-    print(part1(parsed))
-    print(part2(parsed))
+    universe = list(expand(parsed))
+    for row in universe:
+        print(row)
+    print(part1(universe))
+    print(part2(universe))
     print("Part 1:", timeit.timeit(lambda: part1(parsed), number=1))
     print("Part 2:", timeit.timeit(lambda: part2(parsed), number=1))
 
