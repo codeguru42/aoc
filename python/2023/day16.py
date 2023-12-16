28caee77011fab7b745a895bc9fda2ac23ab411c
Code-Apprentice <codeguru42@gmail.com>
2023-12-16 08:30:33 -0700

    AoC 2023, Day 16: Start parsing

--- python/2023/day16.py
+++ python/2023/day16.py
@@ -1,10 +1,45 @@
 import timeit
 
+import networkx as nx
 from aocd import get_data
 
 
 def parse(data):
-    return data.split("\n")
+    lines = data.strip().split("\n")
+    g = nx.DiGraph()
+    height = len(lines)
+    width = len(lines[0])
+    for i, line in enumerate(lines):
+        for j, c in enumerate(line):
+            curr = i, j
+            up = i - 1, j
+            down = i + 1, j
+            left = i, j - 1
+            right = i, j + 1
+            match c:
+                case "|":
+                    if i - 1 >= 0:
+                        g.add_edge(curr, up)
+                    if i + 1 < height:
+                        g.add_edge(curr, down)
+                case "-":
+                    if j - 1 >= 0:
+                        g.add_edge(curr, left)
+                    if j + 1 < width:
+                        g.add_edge(curr, right)
+                case "\\":
+                    if j - 1 >= 0:
+                        g.add_edge(curr, left)
+                    if i + 1 < height:
+                        g.add_edge(curr, down)
+                case "/":
+                    if j + 1 < width:
+                        g.add_edge(curr, right)
+                    if i + 1 < height:
+                        g.add_edge(curr, down)
+                case ".":
+                    pass
+    return lines
 
 
 def part1(lines):
