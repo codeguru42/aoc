3287563882e1ce2db05e854edf4bf9be76b45b1e
Code-Apprentice <codeguru42@gmail.com>
2023-12-23 18:55:20 -0700

    AoC 2023, Day 21: Parse input

--- python/2023/day21.py
+++ python/2023/day21.py
@@ -1,10 +1,27 @@
 import timeit
 
+import networkx as nx
 from aocd import get_data
 
 
+def neighbors(i, j):
+    deltas = (-1, 0, 1)
+    for dr in deltas:
+        for dc in deltas:
+            yield i + dr, j + dc
+
+
 def parse(data):
-    return data.splitlines()
+    lines = data.splitlines()
+    height = len(lines)
+    width = len(lines[0])
+    g = nx.Graph()
+    for i, line in enumerate(lines):
+        for j, cell in enumerate(line):
+            for n_i, n_j in neighbors(i, j):
+                if 0 <= n_i < height and 0 <= n_j < width and lines[n_i][n_j] == ".":
+                    g.add_edge((i, j), (n_i, n_j))
+    return g
 
 
 def part1(lines):
@@ -17,8 +34,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=21)
-    print(data)
     parsed = parse(data)
+    print(parsed)
     print(part1(parsed))
     print(part2(parsed))
     print("Part 1:", timeit.timeit(lambda: part1(parsed), number=1))
