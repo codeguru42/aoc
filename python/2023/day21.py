b557055c8606b39e6ea6c3a41b02df1f1e3d432b
Code-Apprentice <codeguru42@gmail.com>
2023-12-23 19:30:00 -0700

    AoC 2023, Day 21: Build graph with networkx

--- python/2023/day21.py
+++ python/2023/day21.py
@@ -16,30 +16,36 @@ def parse(data):
     height = len(lines)
     width = len(lines[0])
     g = nx.Graph()
+    start = None
     for i, line in enumerate(lines):
         for j, cell in enumerate(line):
+            if lines[i][j] == "S":
+                start = i, j
             for n_i, n_j in neighbors(i, j):
-                if 0 <= n_i < height and 0 <= n_j < width and lines[n_i][n_j] == ".":
+                if (
+                    0 <= n_i < height
+                    and 0 <= n_j < width
+                    and (lines[n_i][n_j] == "." or lines[n_i][n_j] == "S")
+                ):
                     g.add_edge((i, j), (n_i, n_j))
-    return g
+    return start, g
 
 
-def part1(lines):
+def part1(start, g):
     pass
 
 
-def part2(lines):
+def part2(start, g):
     pass
 
 
 def main():
     data = get_data(year=2023, day=21)
-    parsed = parse(data)
-    print(parsed)
-    print(part1(parsed))
-    print(part2(parsed))
-    print("Part 1:", timeit.timeit(lambda: part1(parsed), number=1))
-    print("Part 2:", timeit.timeit(lambda: part2(parsed), number=1))
+    start, g = parse(data)
+    print(part1(start, g))
+    print(part2(start, g))
+    print("Part 1:", timeit.timeit(lambda: part1(start, g), number=1))
+    print("Part 2:", timeit.timeit(lambda: part2(start, g), number=1))
 
 
 if __name__ == "__main__":
