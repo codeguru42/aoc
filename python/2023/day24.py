4c8833f66e8dcfeafacd48d9904a23bd036a450c
Code-Apprentice <codeguru42@gmail.com>
2023-12-24 08:18:56 -0700

    AoC 2023, Day 24: Parse input

--- python/2023/day24.py
+++ python/2023/day24.py
@@ -1,10 +1,22 @@
 import timeit
+from dataclasses import dataclass
 
 from aocd import get_data
 
 
+@dataclass
+class Line:
+    point: tuple[int, int, int]
+    vector: tuple[int, int, int]
+
+
 def parse(data):
-    return data.splitlines()
+    lines = data.splitlines()
+    for line in lines:
+        p, v = line.split("@")
+        point = tuple(int(x.strip()) for x in p.strip().split(","))
+        vector = tuple(int(x.strip()) for x in v.strip().split(","))
+        yield Line(point=point, vector=vector)
 
 
 def part1(lines):
@@ -17,8 +29,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=24)
-    print(data)
-    parsed = parse(data)
+    parsed = list(parse(data))
+    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
