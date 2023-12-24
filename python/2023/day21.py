b13f8aebd3ff0ecf59da7001a5243dad28eb343b
Code-Apprentice <codeguru42@gmail.com>
2023-12-23 18:47:02 -0700

    AoC 2023, Day 21: Stub

---
+++ python/2023/day21.py
@@ -0,0 +1,29 @@
+import timeit
+
+from aocd import get_data
+
+
+def parse(data):
+    return data.splitlines()
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    data = get_data(year=2023, day=21)
+    print(data)
+    parsed = parse(data)
+    print(part1(parsed))
+    print(part2(parsed))
+    print("Part 1:", timeit.timeit(lambda: part1(parsed), number=1))
+    print("Part 2:", timeit.timeit(lambda: part2(parsed), number=1))
+
+
+if __name__ == "__main__":
+    main()
