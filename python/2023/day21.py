580e1880a852d11b1a2beaca2f4b171786d89c85
Code-Apprentice <codeguru42@gmail.com>
2023-12-23 19:47:39 -0700

    AoC 2023, Day 21: Solve Part 1

--- python/2023/day21.py
+++ python/2023/day21.py
@@ -3,6 +3,24 @@ import timeit
 import networkx as nx
 from aocd import get_data
 
+example = """...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
+"""
+
+
+def test_part1():
+    start, g = parse(example)
+    assert part1(start, g, 6) == 16
+
 
 def neighbors(i, j):
     deltas = (-1, 1)
@@ -20,6 +38,8 @@ def parse(data):
     start = None
     for i, line in enumerate(lines):
         for j, cell in enumerate(line):
+            if lines[i][j] == "#":
+                continue
             if lines[i][j] == "S":
                 start = i, j
             for n_i, n_j in neighbors(i, j):
@@ -32,8 +52,9 @@ def parse(data):
     return start, g
 
 
-def part1(start, g):
-    pass
+def part1(start, g, steps):
+    path = nx.single_source_shortest_path_length(g, start, cutoff=steps)
+    return len(list(v for v, l in path.items() if l <= steps and l % 2 == 0))
 
 
 def part2(start, g):
@@ -43,9 +64,9 @@ def part2(start, g):
 def main():
     data = get_data(year=2023, day=21)
     start, g = parse(data)
-    print(part1(start, g))
+    print(part1(start, g, 64))
     print(part2(start, g))
-    print("Part 1:", timeit.timeit(lambda: part1(start, g), number=1))
+    print("Part 1:", timeit.timeit(lambda: part1(start, g, 64), number=1))
     print("Part 2:", timeit.timeit(lambda: part2(start, g), number=1))
 
 
