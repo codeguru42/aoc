e3777f0b06db4ea92624b004f48f757fe951bcc8
Code-Apprentice <codeguru42@gmail.com>
2023-12-24 22:32:42 -0700

    AoC 2023, Day 25: Parse input

--- python/2023/day25.py
+++ python/2023/day25.py
@@ -1,10 +1,17 @@
 import timeit
 
+import networkx as nx
 from aocd import get_data
 
 
 def parse(data):
-    return data.splitlines()
+    g = nx.Graph()
+    lines = data.splitlines()
+    for line in lines:
+        source, destinations = line.split(":")
+        for dest in destinations.strip().split():
+            g.add_edge(source, dest)
+    return g
 
 
 def part1(lines):
@@ -17,8 +24,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=25)
-    print(data)
     parsed = parse(data)
+    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
