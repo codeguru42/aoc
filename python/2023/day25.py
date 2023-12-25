fa8e92fb7ef2930a3357e125bb14a71408ed1e56
Code-Apprentice <codeguru42@gmail.com>
2023-12-24 22:42:30 -0700

    AoC 2023, Day 25: Visualize graph

--- python/2023/day25.py
+++ python/2023/day25.py
@@ -2,11 +2,33 @@ import timeit
 
 import networkx as nx
 from aocd import get_data
+from matplotlib import pyplot as plt
+
+example = """jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr
+"""
+
+
+def test_part1():
+    g = parse(example)
+    nx.draw(g, with_labels=True)
+    plt.show()
 
 
 def parse(data):
     g = nx.Graph()
-    lines = data.splitlines()
+    lines = data.strip().splitlines()
     for line in lines:
         source, destinations = line.split(":")
         for dest in destinations.strip().split():
@@ -25,6 +47,8 @@ def part2(lines):
 def main():
     data = get_data(year=2023, day=25)
     parsed = parse(data)
+    nx.draw(parsed, with_labels=True)
+    plt.show()
     print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
