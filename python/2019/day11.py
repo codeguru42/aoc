bba53b291a0e85ee3bb57deeb9304bb7fd9be327
Code-Apprentice <codeguru42@gmail.com>
2023-12-30 17:04:20 -0700

    AoC 2019, Day 11: Stub

---
+++ python/2019/day11.py
@@ -0,0 +1,27 @@
+import timeit
+
+from aocd import get_data
+
+
+def parse(data):
+    return data.splitlines()
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    data = get_data(year=2019, day=19)
+    print(data)
+    parsed = parse(data)
+    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+
+
+if __name__ == "__main__":
+    main()
