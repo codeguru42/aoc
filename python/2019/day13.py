693ad17fa74b06c698f8a509174126bfc22cd309
Code-Apprentice <codeguru42@gmail.com>
2023-12-31 03:42:19 -0700

    AoC 2019, Day 13: Use aocd and int_code

--- python/2019/day13.py
+++ python/2019/day13.py
@@ -1,6 +1,8 @@
 from collections import Counter
 
-from int_code import run_program
+from aocd import get_data
+
+from int_code import parse, run_program
 
 
 def part1(program):
@@ -15,11 +17,11 @@ def part2(program):
 
 
 def main():
-    with open('day13.txt') as file:
-        int_codes = [int(x) for x in file.readline().split(',')]
-        print(part1(int_codes))
-        print(part2(int_codes))
+    data = get_data(year=2019, day=13)
+    int_codes = parse(data)
+    print(part1(int_codes))
+    print(part2(int_codes))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
