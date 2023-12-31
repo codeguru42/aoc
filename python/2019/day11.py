c3d0452a71e08646e4c5f71f5f25251e424d3cfc
Code-Apprentice <codeguru42@gmail.com>
2023-12-30 17:16:16 -0700

    AoC 2019, Day 11: Parse input

--- python/2019/day11.py
+++ python/2019/day11.py
@@ -2,23 +2,21 @@ import timeit
 
 from aocd import get_data
 
-
-def parse(data):
-    return data.splitlines()
+from int_code import parse
 
 
-def part1(lines):
+def part1(program):
     pass
 
 
-def part2(lines):
+def part2(program):
     pass
 
 
 def main():
     data = get_data(year=2019, day=19)
-    print(data)
     parsed = parse(data)
+    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
