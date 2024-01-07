839263d337ed4512461ca59f76e2e805db7375f6
Code-Apprentice <codeguru42@gmail.com>
2024-01-07 14:00:47 -0700

    AoC 2023, Day 3: Refactor

--- python/2023/day03.py
+++ python/2023/day03.py
@@ -1,7 +1,6 @@
 import math
 from dataclasses import dataclass
 
-import pytest
 from aocd import get_data
 
 example_schematic = """467..114..
@@ -17,55 +16,54 @@ example_schematic = """467..114..
 """
 
 
-@pytest.fixture
-def schematic():
-    return parse(example_schematic)
-
-
-def test_parse_part_numbers(schematic):
-    pns = parse_part_numbers(schematic)
+def test_parse_part_numbers():
+    schematic, parts = parse(example_schematic)
     expected = [
-        PartNumber(part_number=467, position=(0, 0)),
-        PartNumber(part_number=114, position=(0, 5)),
-        PartNumber(part_number=35, position=(2, 2)),
-        PartNumber(part_number=633, position=(2, 6)),
-        PartNumber(part_number=617, position=(4, 0)),
-        PartNumber(part_number=58, position=(5, 7)),
-        PartNumber(part_number=592, position=(6, 2)),
-        PartNumber(part_number=755, position=(7, 6)),
-        PartNumber(part_number=664, position=(9, 1)),
-        PartNumber(part_number=598, position=(9, 5)),
+        Part(part_number=467, position=(0, 0)),
+        Part(part_number=114, position=(0, 5)),
+        Part(part_number=35, position=(2, 2)),
+        Part(part_number=633, position=(2, 6)),
+        Part(part_number=617, position=(4, 0)),
+        Part(part_number=58, position=(5, 7)),
+        Part(part_number=592, position=(6, 2)),
+        Part(part_number=755, position=(7, 6)),
+        Part(part_number=664, position=(9, 1)),
+        Part(part_number=598, position=(9, 5)),
     ]
-    assert list(pns) == expected
+    assert list(parts) == expected
 
 
-def test_is_pn(schematic):
-    pn = PartNumber(part_number=467, position=(0, 0))
-    assert is_pn(pn, schematic)
+def test_is_pn():
+    schematic, parts = parse(example_schematic)
+    part = Part(part_number=467, position=(0, 0))
+    assert is_pn(part, schematic)
 
 
-def test_is_not_pn(schematic):
-    pn = PartNumber(part_number=114, position=(0, 5))
-    assert not is_pn(pn, schematic)
+def test_is_not_pn():
+    schematic, parts = parse(example_schematic)
+    part = Part(part_number=114, position=(0, 5))
+    assert not is_pn(part, schematic)
 
 
-def test_part1(schematic):
-    result = part1(schematic)
+def test_part1():
+    schematic, parts = parse(example_schematic)
+    result = part1(schematic, parts)
     expected = 4361
     assert result == expected
 
 
 @dataclass
-class PartNumber:
+class Part:
     part_number: int
     position: tuple[int, int]
 
 
-def parse(data) -> list[str]:
-    return data.strip().split("\n")
+def parse(data) -> tuple[list[str], list[Part]]:
+    lines = data.strip().split("\n")
+    return lines, parse_part_numbers(lines)
 
 
-def parse_part_numbers(schematic) -> list[PartNumber]:
+def parse_part_numbers(schematic) -> list[Part]:
     for i, line in enumerate(schematic):
         part_number = 0
         position = (-1, -1)
@@ -75,13 +73,13 @@ def parse_part_numbers(schematic) -> list[PartNumber]:
                     position = (i, j)
                 part_number = 10 * part_number + int(c)
             elif part_number != 0:
-                yield PartNumber(part_number=part_number, position=position)
+                yield Part(part_number=part_number, position=position)
                 part_number = 0
         if part_number != 0:
-            yield PartNumber(part_number=part_number, position=position)
+            yield Part(part_number=part_number, position=position)
 
 
-def is_pn(pn: PartNumber, schematic: list[str]):
+def is_pn(pn: Part, schematic: list[str]):
     print(pn)
     h = len(schematic)
     w = len(schematic[0])
@@ -101,20 +99,19 @@ def is_pn(pn: PartNumber, schematic: list[str]):
     return result
 
 
-def part1(schematic: list[str]):
-    part_numbers = parse_part_numbers(schematic)
-    return sum(pn.part_number for pn in part_numbers if is_pn(pn, schematic))
+def part1(schematic: list[str], parts: list[Part]):
+    return sum(part.part_number for part in parts if is_pn(part, schematic))
 
 
-def part2(games: list[str]):
+def part2(schematic: list[str], parts: list[Part]):
     pass
 
 
 def main():
     data = get_data(year=2023, day=3)
-    parsed = list(parse(data))
-    print(part1(parsed))
-    print(part2(parsed))
+    schematic, parts = list(parse(data))
+    print(part1(schematic, parts))
+    print(part2(schematic, parts))
 
 
 if __name__ == "__main__":
