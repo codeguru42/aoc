cf3a35bad8ceb323c70b1e447b90ac556c909f04
Code-Apprentice <codeguru42@gmail.com>
2024-01-08 07:54:57 -0700

    AoC 2023, Day 17: Try to parse input into a graph

--- python/2023/day17.py
+++ python/2023/day17.py
@@ -1,10 +1,18 @@
 import timeit
 
+import networkx as nx
 from aocd import get_data
 
 
 def parse(data):
-    return data.split("\n")
+    g = nx.DiGraph()
+    lines = data.splitlines()
+    for j, line in enumerate(lines):
+        for i, c in enumerate(line):
+            for n in neighbors(i, j):
+                g.add_edge((i, j), n, weight=int(c))
+
+    return g
 
 
 def part1(lines):
@@ -17,9 +25,10 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=17)
-    parsed = parse(data)
-    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
-    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+    g = parse(data)
+    print(g)
+    print("Part 1:", timeit.timeit(lambda: print(part1(g)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(g)), number=1))
 
 
 if __name__ == "__main__":
