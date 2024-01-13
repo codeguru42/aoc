a40bc90845d84f1e7396d415a0fa5a0cbba83a31
Code-Apprentice <codeguru42@gmail.com>
2024-01-13 12:40:09 -0700

    AoC 2023, Day 22: Parse input

--- python/2023/day22.py
+++ python/2023/day22.py
@@ -1,10 +1,23 @@
 import timeit
+from dataclasses import dataclass
 
 from aocd import get_data
 
 
+@dataclass
+class Point:
+    x: int
+    y: int
+    z: int
+
+
 def parse(data):
-    return data.splitlines()
+    lines = data.splitlines()
+    for line in lines:
+        top, bottom = line.split("~")
+        x1, y1, z1 = top.split(",")
+        x2, y2, z2 = bottom.split(",")
+        yield Point(x=x1, y=y1, z=z1), Point(x=x2, y=y2, z=z2)
 
 
 def part1(lines):
@@ -17,10 +30,10 @@ def part2(lines):
 
 def main():
     data = get_data(year=2023, day=22)
-    print(data)
-    parsed = parse(data)
-    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
-    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+    bricks = list(parse(data))
+    print(bricks)
+    print("Part 1:", timeit.timeit(lambda: print(part1(bricks)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(bricks)), number=1))
 
 
 if __name__ == "__main__":
