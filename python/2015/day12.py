245c79c86abc005f4ae207286eafb4a3bf838734
Code-Apprentice <codeguru42@gmail.com>
2024-01-14 17:47:33 -0700

    AoC 2015, Day 12: Parse input

--- python/2015/day12.py
+++ python/2015/day12.py
@@ -1,24 +1,25 @@
+import json
 import timeit
 
 from aocd import get_data
 
 
 def parse(data):
-    return data.splitlines()
+    return json.loads(data)
 
 
-def part1(lines):
+def part1(data):
     pass
 
 
-def part2(lines):
+def part2(data):
     pass
 
 
 def main():
     data = get_data(year=2015, day=12)
-    print(data)
     parsed = parse(data)
+    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
