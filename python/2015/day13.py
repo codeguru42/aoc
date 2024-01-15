3a55b7f097828668a15c04393cfc0f9c8ae177fc
Code-Apprentice <codeguru42@gmail.com>
2024-01-14 19:10:29 -0700

    AoC 2015, Day 13: Parse input

--- python/2015/day13.py
+++ python/2015/day13.py
@@ -1,10 +1,29 @@
+import re
 import timeit
+from dataclasses import dataclass
 
 from aocd import get_data
 
 
+@dataclass
+class Happiness:
+    name: str
+    next_to: str
+    change: int
+
+
 def parse(data):
-    return data.splitlines()
+    lines = data.splitlines()
+    for line in lines:
+        match = re.match(
+            r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+)\.",
+            line,
+        )
+        name, direction, change, next_to = match.groups()
+        change = int(change)
+        if direction == "lose":
+            change = -change
+        yield Happiness(name=name, next_to=next_to, change=change)
 
 
 def part1(lines):
@@ -17,8 +36,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2015, day=13)
-    print(data)
-    parsed = parse(data)
+    parsed = list(parse(data))
+    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
