dc33543cfcbad6506daca3fbd27532a7da84ee57
Code-Apprentice <codeguru42@gmail.com>
2024-11-30 22:13:12 -0700

    AoC 2024, Day 1: Parse input

--- go/2024/day01.go
+++ go/2024/day01.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,10 +18,20 @@ func main() {
 	defer fi.Close()
 
 	scanner := bufio.NewScanner(fi)
+	var left []int
+	var right []int
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		pair := strings.Split(line, " ")
+		p1, _ := strconv.Atoi(pair[0])
+		p2, _ := strconv.Atoi(pair[1])
+		left = append(left, p1)
+		right = append(left, p2)
 	}
+
+	fmt.Println(left)
+	fmt.Println(right)
 
 	if err := scanner.Err(); err != nil {
 		panic(err)
