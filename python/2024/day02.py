a8dd846a38bfce6d0dd4f277c308825d87bf7a70
Code-Apprentice <codeguru42@gmail.com>
2024-12-01 22:21:14 -0700

    AoC 2024, Day 2: (Python) Fix Part 1 and tests

--- python/2024/day02.py
+++ python/2024/day02.py
@@ -1,7 +1,26 @@
 import itertools
 import timeit
 
+import pytest
 from aocd import get_data
+
+
+@pytest.mark.parametrize("nums", [[7, 6, 4, 2, 1], [1, 3, 6, 7, 9]])
+def test_is_safe(nums):
+    assert is_safe(nums)
+
+
+@pytest.mark.parametrize(
+    "nums",
+    [
+        [1, 2, 7, 8, 9],
+        [9, 7, 6, 2, 1],
+        [1, 3, 2, 4, 5],
+        [8, 6, 4, 4, 1],
+    ],
+)
+def test_is_unsafe(nums):
+    assert not is_safe(nums)
 
 
 def parse(data):
@@ -18,7 +37,7 @@ def diffs(nums):
 def is_safe(nums):
     ds = list(diffs(nums))
     return (all(d > 0 for d in ds) or (all(d < 0 for d in ds))) and all(
-        1 <= d <= 3 for d in ds
+        1 <= abs(d) <= 3 for d in ds
     )
 
 
