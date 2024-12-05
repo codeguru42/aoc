e4e9d13e370b5c845c18009522b81fe36b80276b
Code-Apprentice <codeguru42@gmail.com>
2024-12-04 22:16:56 -0700

    AoC 2024, Day 5: (Python) Parse input

--- python/2024/day05.py
+++ python/2024/day05.py
@@ -1,26 +1,42 @@
 import timeit
 
+import networkx as nx
 from aocd import get_data
 
 
+def parse_rules_graph(rules):
+    g = nx.DiGraph()
+    for rule in rules.splitlines():
+        a, b = rule.split("|")
+        g.add_edge(a, b)
+    return g
+
+
+def parse_updates(updates):
+    for line in updates.splitlines():
+        yield line.split(",")
+
+
 def parse(data):
-    return data.splitlines()
+    rules, updates = data.split("\n\n")
+    return parse_rules_graph(rules), list(parse_updates(updates))
 
 
-def part1(lines):
+def part1(lines, updates):
     pass
 
 
-def part2(lines):
+def part2(lines, updates):
     pass
 
 
 def main():
     data = get_data(year=2024, day=5)
-    print(data)
-    parsed = parse(data)
-    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
-    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+    rules, updates = parse(data)
+    print(rules)
+    print(updates)
+    print("Part 1:", timeit.timeit(lambda: print(part1(rules, updates)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(rules, updates)), number=1))
 
 
 if __name__ == "__main__":
