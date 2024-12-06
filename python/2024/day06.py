222c064205c063272b8ae07304dc73f48d026e04
Code-Apprentice <codeguru42@gmail.com>
2024-12-05 22:26:07 -0700

    AoC 2024, Day 5: (Python) Parse input

--- python/2024/day06.py
+++ python/2024/day06.py
@@ -3,24 +3,32 @@ import timeit
 from aocd import get_data
 
 
+def find_start(lines):
+    for x, line in enumerate(lines):
+        for y, c in enumerate(line):
+            if c == "^":
+                return x, y
+
+
 def parse(data):
-    return data.splitlines()
+    lines = data.splitlines()
+    x, y = find_start(lines)
+    return x, y, lines
 
 
-def part1(lines):
+def part1(x, y, m):
     pass
 
 
-def part2(lines):
+def part2(x, y, m):
     pass
 
 
 def main():
     data = get_data(year=2024, day=6)
-    print(data)
-    parsed = parse(data)
-    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
-    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+    x, y, m = parse(data)
+    print("Part 1:", timeit.timeit(lambda: print(part1(x, y, m)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(x, y, m)), number=1))
 
 
 if __name__ == "__main__":
