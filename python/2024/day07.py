569bea808c28a2053baede64b8fa99e7fd694c9a
Code-Apprentice <codeguru42@gmail.com>
2024-12-07 08:31:26 -0700

    AoC 2024, Day 7 (Python): Attempt to solve Part 1

--- python/2024/day07.py
+++ python/2024/day07.py
@@ -14,8 +14,16 @@ def parse(data):
     return list(parse_equations(lines))
 
 
-def part1(lines):
-    pass
+def is_valid(result, terms):
+    if len(terms) == 1:
+        return result == terms[0]
+    return is_valid(result - terms[0], terms[1:]) or is_valid(
+        result // terms[0], terms[1:]
+    )
+
+
+def part1(equations):
+    return sum(result for result, terms in equations if is_valid(result, terms))
 
 
 def part2(lines):
@@ -24,10 +32,9 @@ def part2(lines):
 
 def main():
     data = get_data(year=2024, day=7)
-    parsed = parse(data)
-    print(parsed)
-    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
-    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+    equations = parse(data)
+    print("Part 1:", timeit.timeit(lambda: print(part1(equations)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(equations)), number=1))
 
 
 if __name__ == "__main__":
