5ee1570da86f295bb05854cf42e4a1f90b9e937e
Code-Apprentice <codeguru42@gmail.com>
2024-12-07 08:49:22 -0700

    AoC 2024, Day 7 (Python): Subtract and divide from right to left

--- python/2024/day07.py
+++ python/2024/day07.py
@@ -1,6 +1,27 @@
 import timeit
 
+import pytest
 from aocd import get_data
+
+test_input = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+"""
+
+
+@pytest.fixture
+def equations():
+    return parse(test_input)
+
+
+def test_part1(equations):
+    assert part1(equations) == 3749
 
 
 def parse_equations(lines):
@@ -14,11 +35,32 @@ def parse(data):
     return list(parse_equations(lines))
 
 
+@pytest.mark.parametrize(
+    "equation,expected",
+    zip(
+        parse(test_input),
+        [
+            True,
+            True,
+            False,
+            False,
+            False,
+            False,
+            False,
+            False,
+            True,
+        ],
+    ),
+)
+def test_is_valid(equation, expected):
+    assert is_valid(equation[0], equation[1]) == expected
+
+
 def is_valid(result, terms):
     if len(terms) == 1:
         return result == terms[0]
-    return is_valid(result - terms[0], terms[1:]) or is_valid(
-        result // terms[0], terms[1:]
+    return is_valid(result - terms[-1], terms[:-1]) or is_valid(
+        result // terms[-1], terms[:-1]
     )
 
 
