43b5e3dbbfaa2eece3fde711388a1ffb828dada7
Code-Apprentice <codeguru42@gmail.com>
2024-12-09 20:54:38 -0700

    AoC 2024, Day 9 (Python): Build disk

--- python/2024/day09.py
+++ python/2024/day09.py
@@ -2,13 +2,25 @@ import timeit
 
 from aocd import get_data
 
+import aoc
+
 
 def parse(data):
-    return [int(x) for x in data]
+    return [int(x) for x in data.strip()]
 
 
-def part1(lines):
-    pass
+def build_disk(disk_map):
+    for file_id, (file_size, free_size) in enumerate(aoc.grouper(disk_map, 2)):
+        for _ in range(file_size):
+            yield file_id
+        if free_size:
+            for _ in range(free_size):
+                yield "."
+
+
+def part1(disk_map):
+    disk = list(build_disk(disk_map))
+    print(disk)
 
 
 def part2(lines):
@@ -19,6 +31,7 @@ def main():
     data = get_data(year=2024, day=9)
     parsed = parse(data)
     print(parsed)
+    print(sum(parsed))
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
