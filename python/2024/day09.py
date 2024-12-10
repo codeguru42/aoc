e0c4728c76167b7ad94d6018b4139fe84dacd21a
Code-Apprentice <codeguru42@gmail.com>
2024-12-09 21:14:46 -0700

    AoC 2024, Day 9 (Python): Solve Part 1

--- python/2024/day09.py
+++ python/2024/day09.py
@@ -18,9 +18,23 @@ def build_disk(disk_map):
                 yield "."
 
 
+def move_blocks(disk):
+    while len(disk) > 0:
+        match disk:
+            case [*rest, "."]:
+                disk = rest
+            case [".", *rest, last]:
+                yield last
+                disk = rest
+            case [block, *rest]:
+                yield block
+                disk = rest
+
+
 def part1(disk_map):
     disk = list(build_disk(disk_map))
-    print(disk)
+    defragged = list(move_blocks(disk))
+    return sum(i * file_id for i, file_id in enumerate(defragged))
 
 
 def part2(lines):
@@ -30,8 +44,6 @@ def part2(lines):
 def main():
     data = get_data(year=2024, day=9)
     parsed = parse(data)
-    print(parsed)
-    print(sum(parsed))
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
