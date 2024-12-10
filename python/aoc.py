2f999a447b07d51c5f77f8130d78ae03409f7b10
Code-Apprentice <codeguru42@gmail.com>
2024-12-09 20:08:49 -0700

    AoC 2024, Day 9 (Python): Reusable grouper() function

--- python/aoc.py
+++ python/aoc.py
@@ -1,11 +1,27 @@
 import collections
-from itertools import islice
+import itertools
 
 
 def sliding_window(iterable, n):
     # sliding_window('ABCDEFG', 4) --> ABCD BCDE CDEF DEFG
     it = iter(iterable)
-    window = collections.deque(islice(it, n - 1), maxlen=n)
+    window = collections.deque(itertools.islice(it, n - 1), maxlen=n)
     for x in it:
         window.append(x)
         yield tuple(window)
+
+
+def grouper(iterable, n, *, incomplete="fill", fillvalue=None):
+    "Collect data into non-overlapping fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, fillvalue='x') --> ABC DEF Gxx
+    # grouper('ABCDEFG', 3, incomplete='strict') --> ABC DEF ValueError
+    # grouper('ABCDEFG', 3, incomplete='ignore') --> ABC DEF
+    args = [iter(iterable)] * n
+    if incomplete == "fill":
+        return itertools.zip_longest(*args, fillvalue=fillvalue)
+    if incomplete == "strict":
+        return zip(*args, strict=True)
+    if incomplete == "ignore":
+        return zip(*args)
+    else:
+        raise ValueError("Expected fill, strict, or ignore")
