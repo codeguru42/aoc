ec8c4bf5062a055431432b2e235ec841f58f1f62
Code-Apprentice <codeguru42@gmail.com>
2024-12-10 20:13:09 -0700

    AoC 2024, Day 10 (Python): Attempt to solve Part 1

--- python/2024/day10.py
+++ python/2024/day10.py
@@ -9,8 +9,36 @@ def parse(data):
         yield [int(x) for x in line]
 
 
+def neighbors(r, c):
+    deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for delta in deltas:
+        yield r + delta[0], c + delta[1]
+
+
+def is_in_bounds(r, c, row_count, col_count):
+    return 0 <= r < row_count and 0 <= c < col_count
+
+
+def is_trailhead(r, c, lines):
+    row_count = len(lines)
+    col_count = len(lines[0])
+    for height in range(1, 10):
+        if not any(
+            is_in_bounds(n_r, n_c, row_count, col_count) and lines[n_r][n_c] == height
+            for n_r, n_c in neighbors(r, c)
+        ):
+            return False
+    return True
+
+
 def part1(lines):
-    pass
+    count = 0
+    for r, line in enumerate(lines):
+        for c, v in enumerate(line):
+            if v == 0:
+                if is_trailhead(r, c, lines):
+                    count += 1
+    return count
 
 
 def part2(lines):
