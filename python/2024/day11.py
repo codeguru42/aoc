20c28dd68afc8729215028a9e37afaa6cd73c7ef
Code-Apprentice <codeguru42@gmail.com>
2024-12-12 19:20:21 -0700

    AoC 2024, Day 11 (Python): Stub

---
+++ python/2024/day11.py
@@ -0,0 +1,28 @@
+import timeit
+
+from aocd import get_data
+
+
+def parse(data):
+    return data.splitlines()
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+def main():
+    with open("day11.txt") as f:
+        data = f.read()
+    print(data)
+    parsed = parse(data)
+    print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
+    print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
+
+
+if __name__ == "__main__":
+    main()
