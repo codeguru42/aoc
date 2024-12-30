10ab3a77538aac2f1fc04f06ce45154359e452ac
Code-Apprentice <codeguru42@gmail.com>
2024-12-29 23:23:10 -0700

    AoC 2024, Day 14 (Python): Parse input

--- python/2024/day14.py
+++ python/2024/day14.py
@@ -1,10 +1,30 @@
+import re
 import timeit
+from dataclasses import dataclass
 
 from aocd import get_data
 
 
+@dataclass
+class Point:
+    x: int
+    y: int
+
+
+@dataclass
+class Robot:
+    p: Point
+    v: Point
+
+
 def parse(data):
-    return data.splitlines()
+    lines = data.strip().splitlines()
+    for line in lines:
+        match = re.match(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)", line.strip())
+        yield Robot(
+            Point(int(match.group(1)), int(match.group(2))),
+            Point(int(match.group(3)), int(match.group(4))),
+        )
 
 
 def part1(lines):
@@ -17,8 +37,8 @@ def part2(lines):
 
 def main():
     data = get_data(year=2024, day=14)
-    print(data)
-    parsed = parse(data)
+    parsed = list(parse(data))
+    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
