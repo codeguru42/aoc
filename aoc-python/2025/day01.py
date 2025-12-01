c18970974b59bf9a8b550a2030e50a746b62ef4e
codeguru42 <codeguru42@gmail.com>
2025-11-30 22:08:59 -0700

    AoC 2025, Day 1: Solve Part 1

--- aoc-python/2025/day01.py
+++ aoc-python/2025/day01.py
@@ -4,11 +4,26 @@ from aocd import get_data
 
 
 def parse(data):
-    return data.splitlines()
+    for line in data.splitlines():
+        first = line.strip()[0]
+        rest = line.strip()[1:]
+        match first:
+            case "R":
+                yield int(rest)
+            case "L":
+                yield -int(rest)
+            case _:
+                raise ValueError(f"Unexpected line {line}")
 
 
 def part1(lines):
-    pass
+    position = 50
+    count = 0
+    for line in lines:
+        position += line
+        if position % 100 == 0:
+            count += 1
+    return count
 
 
 def part2(lines):
@@ -17,8 +32,7 @@ def part2(lines):
 
 def main():
     data = get_data(year=2025, day=1)
-    print(data)
-    parsed = parse(data)
+    parsed = list(parse(data))
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
