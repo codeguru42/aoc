e29bafec610100beb32d893b473d62979868272c
codeguru42 <codeguru42@gmail.com>
2025-12-02 22:23:01 -0700

    AoC 2025, Day 3: First attempt to solve Part 1

--- aoc-python/2025/day03.py
+++ aoc-python/2025/day03.py
@@ -1,3 +1,4 @@
+import operator
 import timeit
 
 from aocd import get_data
@@ -8,8 +9,22 @@ def parse(data):
         yield [int(char) for char in line.strip()]
 
 
+def max_digit(line):
+    if len(line) == 1:
+        return line[0], []
+    i, m = max(enumerate(line[:-1]), key=operator.itemgetter(1))
+    return m, line[i + 1 :]
+
+
+def joltages(lines):
+    for line in lines:
+        m1, rest = max_digit(line)
+        m2, _ = max_digit(rest)
+        yield 10 * m1 + m2
+
+
 def part1(lines):
-    pass
+    return sum(joltages(lines))
 
 
 def part2(lines):
@@ -19,7 +34,6 @@ def part2(lines):
 def main():
     data = get_data(year=2025, day=3)
     parsed = list(parse(data))
-    print(parsed)
     print("Part 1:", timeit.timeit(lambda: print(part1(parsed)), number=1))
     print("Part 2:", timeit.timeit(lambda: print(part2(parsed)), number=1))
 
